de1edc408c0adb742789e8c40ecb681d16b57d9b
asmyasikova83 <asmyasnikova83@gmail.com>
2020-07-19 21:01:45 +0300

    Add config and muscular artifact correction

---
+++ config.py
@@ -0,0 +1,54 @@
+import os
+
+stim = "reinforced"
+
+L_freq = 4
+H_freq = 6
+f_step = 1
+
+period_start = -1.000 # epoch start
+period_end = 1.000 #epoch end
+
+baseline = (-0.350, -0.050) # time interval before the appearance of the fixation cross
+
+subjects = [
+    'P000',
+    'P001',
+    'P002',
+    'P003',
+    'P004',
+    'P005',
+    'P006',
+    'P007',
+    'P008',
+    'P009',
+    'P010',
+    'P011',
+    'P012',
+    'P014',
+    'P015',
+    'P016',
+    'P017',
+    'P018',
+    'P019',
+    'P020',
+    'P021',
+    'P022',
+    'P023',
+    'P024',
+    'P025',
+    'P026',
+    'P028',
+    'P029',
+    'P030']
+
+runs = ['1','2','3','4','5','6','7']
+
+'''
+subjects = [
+    'P000',
+    'P001']
+
+runs = ['1','2']
+'''
+
---
+++ mio_correction.py
@@ -0,0 +1,87 @@
+import mne, os
+import numpy as np
+from config import *
+import pathlib
+
+os.makedirs("baseline", exist_ok=True)
+os.makedirs("Cleaned_events", exist_ok=True)
+ 
+def no_mio_events(epochs_ar, thres):
+    #N_events, N_chans, N_times
+    epochs_ar = epochs_ar.swapaxes(0, 1)
+    epochs_ar = epochs_ar.swapaxes(1, 2)
+    N_chans, N_times, N_events = epochs_ar.shape
+    
+    EV_DATA = np.abs(epochs_ar)
+    
+    CHAN_MAX = np.max(EV_DATA, axis=1)
+    RESH_DATA = EV_DATA.reshape(N_chans,  N_events* N_times) 
+    
+    CHAN_MEAN = np.mean(RESH_DATA, axis=1)
+    CHAN_STD = np.std(RESH_DATA, axis=1)
+    
+    good_chan = []
+    for i in range(N_events):
+        select = np.where(CHAN_MAX[:, i] > CHAN_MEAN + thres*CHAN_STD)
+        if select[0].shape[0] <= N_chans//4:
+            good_chan.append(i)
+    return np.array(good_chan) 
+
+data_path = '/home/asmyasnikova83/DATA/links/'
+out_path = f'theta_reinforced_{L_freq}_{H_freq}/'
+os.makedirs(os.path.join(os.getcwd(), "Sensor", "TFR", out_path), exist_ok = True)
+fpath_raw = '/home/asmyasnikova83/DATA/links/{}/{}_run{}_raw_tsss_mc.fif'
+fpath_events = '/home/asmyasnikova83/DATA/reinforced/{}_run{}_events.txt'
+
+'''
+with open("config.py", "r") as f_in:
+    settings = f_in.readlines()
+    with open(os.path.join(os.getcwd(), "Sensor", "TFR", out_path, "config.log"), "w") as f_out:
+        f_out.writelines(settings)
+'''
+
+def calculate_beta(subj, run, fpath_raw):
+    freqs = np.arange(L_freq, H_freq, f_step)
+        
+    raw_data = mne.io.Raw(fpath_raw.format(subj, subj, run), preload=False)
+
+    picks = mne.pick_types(raw_data.info, meg = True)
+    events_raw = mne.find_events(raw_data, stim_channel='STI101', output='onset', consecutive='increasing', min_duration=0, shortest_event=1, mask=None, uint_cast=False, mask_type='and', initial_event=False, verbose=None)
+    fpath_events = '/home/asmyasnikova83/DATA/reinforced/{}_run{}_events.txt'
+    events = np.loadtxt(fpath_events.format(subj, run), dtype=int)
+    epochs = mne.Epochs(raw_data, events, event_id = None, tmin = period_start, tmax = period_end, picks=picks, preload = True)
+    epochs = epochs.pick(picks="grad")
+
+    thres = 7 #procedure to remove epochs-outliers based on thres*STD difference
+    clean = no_mio_events(epochs.get_data(), thres)
+   
+    print(subj, thres, str(events.shape[0]) + " ~~~~ " + str(events[clean].shape[0]) )
+    cleaned = events[clean]
+    print(cleaned)
+    del epochs, events, clean, picks, raw_data
+
+    full_ev = []
+    for i in range(cleaned.shape[0]):
+        event_ind = events_raw.tolist().index(cleaned[i].tolist())
+        full_ev.append(events_raw[event_ind].tolist())
+        event_ind += 1
+
+    del events_raw
+    evee = list(map(str, full_ev))
+    evee = list(map(lambda x: x + "\n", evee))
+    with open(f"Cleaned_events/{subject}_run{run}_cleaned_events.txt", "w") as ff:
+        ff.writelines(evee)
+
+for run in runs:
+    for subject in subjects:
+
+        rf = fpath_events.format(subject, run)
+        print(rf)
+        file = pathlib.Path(rf)
+        if file.exists():
+            print('This file is being processed: ', rf)
+            calculate_beta(subject, run, fpath_raw)
+        else:
+            print('This file: ', rf, 'does not exit')
+            continue
+
