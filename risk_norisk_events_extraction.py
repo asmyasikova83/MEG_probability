d4c1f1c30034662b034a4a7e3441c4bd1dda5152
asmyasikova83 <asmyasnikova83@gmail.com>
2020-10-27 13:47:49 +0300

    fixies in risk norisk events extraction

--- risk_norisk_events_extraction.py
+++ risk_norisk_events_extraction.py
@@ -9,11 +9,6 @@ fpath_events_prerisk = '/home/asmyasnikova83/DATA/reinforced/{}_run{}_events_pre
 fpath_events_postrisk = '/home/asmyasnikova83/DATA/reinforced/{}_run{}_events_postrisk.txt'
 fpath_log = '/home/asmyasnikova83/DATA/reinforced/{}_run{}_log_risk_norisk.txt'
 
-risk_trained = []
-norisk_trained = []
-prerisk_trained = []
-postrisk_trained = []
-
 for run in runs:
     for subject in subjects:
         print(fpath.format(subject, run, subject))                                                                                                                                                                 
@@ -42,35 +37,6 @@ for run in runs:
                 else:
                     correct_count = correct_count + 1
                 #risk
-                
-                if trained and events[i-1][2] == 40 or trained and events[i-1][2] == 41 or trained and events[i-1][2] == 46 or trained and events[i-1][2] == 47:
-                    if trained and events[i][2] == 42 or trained and events[i][2] == 43 or trained and events[i][2] == 44 or trained and events[i][2] == 45:
-                        print('risk')
-                        if trained and events[i+1][2] == 40 or trained and events[i+1][2] == 41 or trained and events[i+1][2] == 46 or trained and events[i+1][2] == 47:
-                            risk.append(events[i])
-                            log_risk_norisk.append('risk')
-          
-                if trained and events[i-1][2] == 40 or trained and events[i-1][2] == 41 or trained and events[i-1][2] == 46 or trained and events[i-1][2] == 47:
-                    if trained and events[i][2] == 40 or trained and events[i][2] == 41 or trained and events[i][2] == 46 or trained and events[i][2] == 47:
-                        print('norisk')
-                        if trained and events[i+1][2] == 40 or trained and events[i+1][2] == 41 or trained and events[i+1][2] == 46 or trained and events[i+1][2] == 47:
-                            norisk.append(events[i])
-                            log_risk_norisk.append('norisk')
-
-                if trained and events[i-1][2] == 40 or trained and events[i-1][2] == 41 or trained and events[i-1][2] == 46 or trained and events[i-1][2] == 47:
-                    if trained and events[i][2] == 40 or trained and events[i][2] == 41 or trained and events[i][2] == 46 or trained and events[i][2] == 47:
-                        if trained and events[i+1][2] == 42 or trained and events[i+1][2] == 43 or trained and events[i+1][2] == 44 or trained and events[i+1][2] == 45:
-                            print('prerisk')
-                            prerisk.append(events[i])
-                            log_risk_norisk.append('prerisk')
-
-                if trained and events[i-1][2] == 42 or trained and events[i-1][2] == 43 or trained and events[i-1][2] == 44 or trained and events[i-1][2] == 45:
-                    if trained and events[i][2] == 40 or trained and events[i][2] == 41 or trained and events[i][2] == 46 or trained and events[i][2] == 47:
-                        if trained and events[i+1][2] == 40 or trained and events[i+1][2] == 41 or trained and events[i+1][2] == 46 or trained and events[i+1][2] == 47:
-                            print('postrisk')
-                            postrisk.append(events[i])
-                            log_risk_norisk.append('postrisk')
-                
             if events[i][2] == 42 or events[i][2] == 43 or events[i][2] == 44 or events[i][2] == 45: #incorrect response
                 if trained:
                     res.append(events[i])
@@ -83,52 +49,93 @@ for run in runs:
                 res.append(events[i])
                 log.append('correct')
            
-                if trained and events[i-1][2] == 40 or trained and events[i-1][2] == 41 or trained and events[i-1][2] == 46 or trained and events[i-1][2] == 47:
-                    if trained and events[i][2] == 42 or trained and events[i][2] == 43 or trained and events[i][2] == 44 or trained and events[i][2] == 45:
-                        print('risk')
-                        if trained and events[i+1][2] == 40 or trained and events[i+1][2] == 41 or trained and events[i+1][2] == 46 or trained and events[i+1][2] == 47:
-                            risk.append(events[i])
+            if trained and events[i - 1][2] == 40 or trained and events[i - 1][2] == 41 or trained and events[i - 1][2] == 46 or trained and events[i - 1][2] == 47:
+                if i + 7 >= len(events):
+                    continue
+                str_digit1 = str(events[i + 3][2])
+                str_digit2 = str(events[i + 4][2])
+                d1 = [int(d) for d in str_digit1]
+                d2 = [int(d) for d in str_digit2]
+                if d1 == 4 and d2 == 4:
+                    continue
+                else:
+                    if events[i + 3][2] == 42 or events[i + 3][2] == 43  or events[i + 3][2] == 43 or events[i + 3][2] == 44 or events[i + 3][2] == 45:
+                        print('risk:2d step')
+                        if events[i + 7][2] == 40 or events[i + 7][2] == 41  or events[i + 7][2] == 46 or events[i + 7][2] == 47:
+                            risk.append(events[i + 3])
+                            print('risk: 3d step')
                             log_risk_norisk.append('risk') 
               
-                if trained and events[i-1][2] == 40 or trained and events[i-1][2] == 41 or trained and events[i-1][2] == 46 or trained and events[i-1][2] == 47:
-                    if trained and events[i][2] == 40 or trained and events[i][2] == 41 or trained and events[i][2] == 46 or trained and events[i][2] == 47:
-                        print('norisk')
-                        if trained and events[i+1][2] == 40 or trained and events[i+1][2] == 41 or trained and events[i+1][2] == 46 or trained and events[i+1][2] == 47:
-                            norisk.append(events[i])
+            if trained and events[i - 1][2] == 40 or trained and events[i - 1][2] == 41 or trained and events[i - 1][2] == 46 or trained and events[i - 1][2] == 47:
+                if i + 7 >= len(events):
+                    continue
+                str_digit1 = str(events[i + 3][2])
+                str_digit2 = str(events[i + 4][2])
+                d1 = [int(d) for d in str_digit1]
+                d2 = [int(d) for d in str_digit2]
+                if d1 == 4 and d2 == 4:
+                    continue
+                else:
+                    if events[i + 3][2] == 40 or events[i + 3][2] == 41 or events[i + 3][2] == 46 or events[i + 3][2] == 47:
+                        print('norisk: 2d step')
+                        if events[i + 7][2] == 40 or events[i + 7][2] == 41 or events[i + 1][2] == 46 or events[i + 7][2] == 47:
+                            print('norisk: 3d step')
+                            norisk.append(events[i + 3])
                             log_risk_norisk.append('norisk')
-
-                if trained and events[i-1][2] == 40 or trained and events[i-1][2] == 41 or trained and events[i-1][2] == 46 or trained and events[i-1][2] == 47:
-                    if trained and events[i][2] == 40 or trained and events[i][2] == 41 or trained and events[i][2] == 46 or trained and events[i][2] == 47:
-                        if trained and events[i+1][2] == 42 or trained and events[i+1][2] == 43 or trained and events[i+1][2] == 44 or trained and events[i+1][2] == 45:
-                            print('prerisk')
-                            prerisk.append(events[i])
+  
+            if trained and events[i - 1][2] == 40 or trained and events[i - 1][2] == 41 or trained and events[i - 1][2] == 46 or trained and events[i - 1][2] == 47:
+                if i + 7 >= len(events):
+                    continue
+                str_digit1 = str(events[i + 3][2])
+                str_digit2 = str(events[i + 4][2])
+                d1 = [int(d) for d in str_digit1]
+                d2 = [int(d) for d in str_digit2]
+                if d1 == 4 and d2 == 4:
+                    continue
+                else:
+                    if events[i + 3][2] == 40 or events[i + 3][2] == 41 or events[i + 3][2] == 46 or events[i + 3][2] == 47:
+                        print('prerisk: 2d step')
+                        if events[i + 7][2] == 42 or events[i + 7][2] == 43 or events[i + 7][2] == 44 or events[i + 7][2] == 45:
+                            print('prerisk: 3d step')
+                            prerisk.append(events[i + 3])
                             log_risk_norisk.append('prerisk')
 
-                if trained and events[i-1][2] == 42 or trained and events[i-1][2] == 43 or trained and events[i-1][2] == 44 or trained and events[i-1][2] == 45:
-                    if trained and events[i][2] == 40 or trained and events[i][2] == 41 or trained and events[i][2] == 46 or trained and events[i][2] == 47:
-                        if trained and events[i+1][2] == 40 or trained and events[i+1][2] == 41 or trained and events[i+1][2] == 46 or trained and events[i+1][2] == 47:
-                            print('postrisk')
-                            postrisk.append(events[i])
+            if trained and events[i - 1][2] == 42 or trained and events[i - 1][2] == 43 or trained and events[i - 1][2] == 44 or trained and events[i - 1][2] == 45:
+                print('len events', len(events))
+                if i + 7 >= len(events):
+                    print('Ready to continue')
+                    continue
+                str_digit1 = str(events[i + 3][2])
+                str_digit2 = str(events[i + 4][2])
+                d1 = [int(d) for d in str_digit1]
+                d2 = [int(d) for d in str_digit2]
+                if d1 == 4 and d2 == 4:
+                    continue
+                else:
+                    if events[i + 3][2] == 40 or events[i + 3][2] == 41 or events[i + 3][2] == 46 or events[i + 3][2] == 47:
+                        print('postrisk: 2d step')
+                        if events[i + 7][2] == 40 or events[i + 7][2] == 41 or events[i + 7][2] == 46 or events[i+7][2] == 47:
+                            print('postrisk: 3d step')
+                            postrisk.append(events[i + 3])
                             log_risk_norisk.append('postrisk')
  
         if len(res) != 0:
             answer_count = correct_counter/len(res)
             print(answer_count)
             if answer_count > 0.66:
-                norisk_trained.extend(norisk)
-                risk_trained.extend(risk)
-                prerisk_trained.extend(prerisk)
-                postrisk_trained.extend(postrisk)
                 events_norisk  = open(fpath_events_norisk.format(subject, run), "w")
                 events_risk = open(fpath_events_risk.format(subject, run), "w")
                 events_prerisk = open(fpath_events_prerisk.format(subject, run), "w")
                 events_postrisk = open(fpath_events_postrisk.format(subject, run), "w")
                 log_file = open(fpath_log.format(subject, run), "w")
-  
-                np.savetxt(events_norisk, norisk_trained, fmt = "%d")
-                np.savetxt(events_risk, risk_trained, fmt = "%d")
-                np.savetxt(events_prerisk, prerisk_trained, fmt = "%d")
-                np.savetxt(events_postrisk, postrisk_trained, fmt = "%d")
+                print('events norisk', norisk)
+                print('events risk', risk)
+                print('events prerisk', prerisk)
+                print('events postrisk', postrisk)
+                np.savetxt(events_norisk, norisk, fmt = "%d")
+                np.savetxt(events_risk, risk, fmt = "%d")
+                np.savetxt(events_prerisk, prerisk, fmt = "%d")
+                np.savetxt(events_postrisk, postrisk, fmt = "%d")
                 log_risk_norisk_str='\n'.join(log_risk_norisk)
                 log_file.write(log_risk_norisk_str)
 
