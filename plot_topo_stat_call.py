edb01905a903acab3e7b17d770aa53413210a9ba
asmyasikova83 <asmyasnikova83@gmail.com>
2020-12-22 11:36:02 +0300

    new fdr for topo

--- plot_topo_stat_call.py
+++ plot_topo_stat_call.py
@@ -8,6 +8,7 @@ import statsmodels.stats.multitest as mul
 from config import *
 import pathlib
 from plot_time_course_in_html_functions import clear_html
+from plot_time_course_in_html_functions import to_str_ar
 from plot_time_course_in_html_functions import add_str_html
 from plot_time_course_in_html_functions import add_pic_topo_html
 from plot_time_course_in_html_functions import get_short_legend
@@ -15,17 +16,20 @@ from plot_time_course_in_html_functions import delaete_bad_sub
 from plot_time_course_in_html_functions import p_val_binary
 from plot_time_course_in_html_functions import space_fdr
 from compute_p_val import compute_p_val
+from tfce import *
+from scipy import stats, io
+import statsmodels.stats.multitest as mul
 
 path = os.getcwd()
 list_files = os.listdir(path)
 
-output = 'output_topo/'
+output = 'output_topo_fdr/'
 
 
 topomaps = [f'{legend[0]}',
             f'{legend[1]}',
-            'difference',
-            'difference_with_fdr']
+            'difference_fdr',
+            'difference_deep_fdr']
 options = {
     'page-size':'A3',
     'orientation':'Landscape',
@@ -37,77 +41,108 @@ os.makedirs(os.path.join(output, f'{legend[0]}_vs_{legend[1]}'), exist_ok=True)
 
 #donor data file
 temp = mne.Evoked(f'{prefix}donor-ave.fif')
-temp.times = np.arange(-2.000, 1.502, 0.004)
-times_to_plot = np.arange(-2.0, 1.5, 0.2)
+temp.times = time
 
 comp1_mean, comp2_mean, contr, temp1, temp2, p_val, binary, subjects1  = compute_p_val(subjects, kind, train, frequency, check_num_sens)
-p_val_fdr = space_fdr(p_val)
-#legend = ["Norisk", "Risk"]
 rewrite = True
+df1 = contr[:, 0, 204:, :] #per channel
+df2 = contr[:, 1, 204:, :]
+print('df1 shape', df1.shape)
+print('df2 shape', df2.shape)
 
-    ##### CONDITION1 ######
+res_tfce = np.zeros((876, 102))
+pos = io.loadmat(f'{prefix}pos_store.mat')['pos']
+chan_labels = to_str_ar(io.loadmat(f'{prefix}channel_labels.mat')['chanlabels'])
+dict_col = { 'risk': 'salmon', 'norisk': 'olivedrab', 'prerisk': 'mediumpurple' , 'postrisk':'darkturquoise'  }
+p_val_fdr = space_fdr(p_val)
+
+    ##### CONDITION1 and Stat  ######
 # average = 0.1 means averaging of the power data over 100 ms
 
+print('comp1_mean', comp1_mean.shape)
+#t_stat_con1, p_val_con1 = stats.ttest_1samp(comp1, 0, axis=0)
+
+
+print('df1', df1.shape)
+print('time', len(time))
+print('times to plot', len(times_to_plot))
+t_stat_con1, p_val_con1 = stats.ttest_1samp(df1, 0, axis=0)
+print('p val con1 shape', p_val_con1.shape)
+width, height = p_val_con1.shape
+p_val_con1 = p_val_con1.reshape(width * height)
+_, p_val_con1 = mul.fdrcorrection(p_val_con1)
+p_val_con1 = p_val_con1.reshape((width, height))
+binary = p_val_binary(p_val_con1, treshold = 0.05)
+
+
 temp.data = comp1_mean[204:,:]
-fig = temp.plot_topomap(times = times_to_plot, average = 0.1,
+fig = temp.plot_topomap(times = times_to_plot, average = 0.1, units = "dB",
                             scalings = dict(eeg=1e6, grad=1, mag=1e15), 
                             ch_type='planar1', time_unit='s', show = False, 
-                            title = legend[0], colorbar = True, vmax=p_mul_topo, vmin=-p_mul_topo)
+                            title = legend[0] + ' stat fdr against zero', colorbar = True, vmax=p_mul_topo, vmin=-p_mul_topo,
+                            extrapolate="local", mask = np.bool_(binary),
+                            mask_params = dict(marker='o', markerfacecolor='w', markeredgecolor='k',
+                                              linewidth=0, markersize=7, markeredgewidth=2))
 
 fig.savefig(os.path.join(output, legend[0] + '_vs_' + legend[1], legend[0] + '.png'), dpi = 300)
 plt.close()
 
-    ##### CONDITION2 ######
+    ##### CONDITION2 and Stat ######
     
+#temp.data = comp2_mean[204:,:]
+t_stat_con2, p_val_con2 = stats.ttest_1samp(df2, 0, axis=0)
+width, height = p_val_con2.shape
+p_val_con2 = p_val_con2.reshape(width * height)
+_, p_val_con2 = mul.fdrcorrection(p_val_con2)
+p_val_con2 = p_val_con2.reshape((width, height))
+binary = p_val_binary(p_val_con2, treshold = 0.05)
+
 temp.data = comp2_mean[204:,:]
-fig = temp.plot_topomap(times = times_to_plot, average = 0.1,
+fig = temp.plot_topomap(times = times_to_plot, average = 0.1, units = 'dB',
                             scalings = dict(eeg=1e6, grad=1, mag=1e15),
                             ch_type='planar1', time_unit='s', show = False, 
-                            title = legend[1], colorbar = True, vmax=p_mul_topo, vmin=-p_mul_topo)
+                            title = legend[1] + ' stat fdr  against zero', colorbar = True, vmax=p_mul_topo, vmin=-p_mul_topo,
+                            extrapolate="local", mask = np.bool_(binary),
+                            mask_params = dict(marker='o', markerfacecolor='w', markeredgecolor='k',
+                                               linewidth=0, markersize=7, markeredgewidth=2))
+
 fig.savefig(os.path.join(output, legend[0] + '_vs_' + legend[1], legend[1] + '.png'), dpi = 300)
 plt.close()
     
-    ##### CONDITION2 - CONDITION1 with marks (no FDR) ######
-
+    ##### CONDITION2 - CONDITION1 with marks no time  (space FDR) ######
+p_val_fdr = space_fdr(p_val)
+binary_fdr = p_val_binary(p_val_fdr, treshold = 0.05)
 temp.data = comp2_mean[204:,:] - comp1_mean[204:,:]
-fig = temp.plot_topomap(times = times_to_plot, average = 0.1,
+fig = temp.plot_topomap(times = times_to_plot, average = 0.1, units = 'dB',
                             scalings = dict(eeg=1e6, grad=1, mag=1e15), 
                             ch_type='planar1', time_unit='s', show = False, 
-                            title = '%s - %s'%(legend[1], legend[0]), colorbar = True, 
-                            vmax=p_mul_topo_contrast, vmin=-p_mul_topo_contrast, extrapolate="local", mask = np.bool_(binary[204:,:]),
+                            title = '%s - %s  space fdr: marks for each head separately'%(legend[1], legend[0]), colorbar = True,
+                            vmax=p_mul_topo_contrast, vmin=-p_mul_topo_contrast, extrapolate="local", mask = np.bool_(binary_fdr[204:,:]),
                             mask_params = dict(marker='o', markerfacecolor='w', markeredgecolor='k',
                                                linewidth=0, markersize=7, markeredgewidth=2))
-fig.savefig(os.path.join(output, legend[0] + '_vs_' + legend[1],'difference.png'), dpi = 300)
+fig.savefig(os.path.join(output, legend[0] + '_vs_' + legend[1],'difference_fdr.png'), dpi = 300)
 plt.close()
  
-    #### CONDITION2 - CONDITION1 with marks (WITH FDR) ######
+    #### CONDITION2 - CONDITION1 with marks (WITH FDR) deep FDR with time ####
 
-binary_fdr = p_val_binary(p_val_fdr, treshold = 0.05)
-
+t_stat, p_val_deep = stats.ttest_rel(df1, df2, axis=0)
+width, height = p_val_deep.shape
+p_val_resh = p_val_deep.reshape(width * height)
+_, p_val_deep_fdr = mul.fdrcorrection(p_val_resh)
+p_val_deep_fdr = p_val_deep_fdr.reshape((width, height))
+binary_deep_fdr = p_val_binary(p_val_deep_fdr, treshold = 0.05)
 temp.data = comp2_mean[204:,:] - comp1_mean[204:,:]
 
-fig = temp.plot_topomap(times = times_to_plot, average = 0.1,
+fig = temp.plot_topomap(times = times_to_plot, average = 0.1, units = 'dB',
                             scalings = dict(eeg=1e6, grad=1, mag=1e15), 
                             ch_type='planar1', time_unit='s', show = False, 
-                            title = '%s - %s with_fdr'%(legend[1], legend[0]), colorbar = True, 
-                            vmax=p_mul_topo_fdr_contrast, vmin=-p_mul_topo_fdr_contrast, extrapolate="local", mask = np.bool_(binary_fdr[204:,:]),
+                            title = '%s - %s deep fdr over all heads'%(legend[1], legend[0]), colorbar = True,
+                            vmax=p_mul_topo_fdr_contrast, vmin=-p_mul_topo_fdr_contrast, extrapolate="local", mask = np.bool_(binary_deep_fdr),
                             mask_params = dict(marker='o', markerfacecolor='w', markeredgecolor='k',
                                                linewidth=0, markersize=7, markeredgewidth=2))
-fig.savefig(os.path.join(output, legend[0] + '_vs_' + legend[1],'difference_with_fdr.png'), dpi = 300)
+fig.savefig(os.path.join(output, legend[0] + '_vs_' + legend[1],'difference_deep_fdr.png'), dpi = 300)
 plt.close()
     
-
-    ##### CONDITION2 - CONDITION1 cutted by thershold (WITH FDR) ######
-    # ask Nikita about the following formula
-temp.data = (comp2_mean[204:,:] - comp1_mean[204:,:])*binary_fdr[204:,:]
-
-fig = temp.plot_topomap(times = times_to_plot, average = 0.1,
-                            scalings = dict(eeg=1e6, grad=1, mag=1e15), 
-                            ch_type='planar1', time_unit='s', show = False, 
-                            title = '%s - %s (cut) with fdr'%(legend[1], legend[0]), colorbar = True, 
-                            vmax=p_mul_topo_fdr_contrast, vmin=-p_mul_topo_fdr_contrast, extrapolate="local")
-fig.savefig(os.path.join(output, legend[0] + '_vs_' + legend[1],'p_value_cut_with_fdr.png'), dpi = 300)
-plt.close()
    
 
 html_name = os.path.join(output, legend[0] + '_vs_' + legend[1] + '.html')
@@ -115,8 +150,8 @@ clear_html(html_name)
 add_str_html(html_name, '<!DOCTYPE html>')
 add_str_html(html_name, '<html>')
 add_str_html(html_name, '<body>')
-add_str_html(html_name, '<p style="font-size:20px;"><b> %s, average %s, %s, trained, %d subjects </b></p>' % (legend[0] + '_vs_' + legend[1], frequency, stimulus, len(subjects1)))
-add_str_html(html_name, '<p style="font-size:20px;"><b> P_val < 0.05 marked (or saved from cutting) </b></p>' )
+add_str_html(html_name, '<p style="font-size:20px;"><b> %s, %s, %s, trained, %s, %s, %d subjects </b></p>' % (legend[0] + '_vs_' + legend[1], frequency, stimulus, baseline, zero_point, len(subjects1)))
+add_str_html(html_name, '<p style="font-size:20px;"><b> boolean fdr  = 1  marked </b></p>' )
 add_str_html(html_name, '<table>')
 for topo in topomaps:
     add_str_html(html_name, "<tr>")
