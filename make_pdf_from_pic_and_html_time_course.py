0bc76d7dfcfa28561b24ede98509a51b96ea0b1c
asmyasikova83 <alexdsv@yandex.ru>
2021-02-28 19:54:03 +0300

    make_pdf

--- make_pdf_from_pic_and_html_time_course.py
+++ make_pdf_from_pic_and_html_time_course.py
@@ -31,19 +31,19 @@ rewrite = True
 os.makedirs(output, exist_ok=True)
 
 if mode == 'server':
-    pos = io.loadmat('/home/asmyasnikova83/DATA/pos_store.mat')['pos']
-    chan_labels = to_str_ar(io.loadmat('/home/asmyasnikova83/DATA/channel_labels.mat')['chanlabels'])
+    pos = io.loadmat(path_home + 'pos_store.mat')['pos']
+    chan_labels = to_str_ar(io.loadmat(path_home + 'channel_labels.mat')['chanlabels'])
 else:
     pos = io.loadmat('/home/sasha/MEG/GITHUB/MEG_probability/pos_store.mat')['pos']
     chan_labels = to_str_ar(io.loadmat('/home/sasha/MEG/GITHUB/MEG_probability/channel_labels.mat')['chanlabels'])    
 
 path = os.getcwd()
 
-html_name = '/home/sasha/MEG/GITHUB/MEG_probability/output/pic_compose_combine_planar_Norisk_vs_Risk_all.html'
-pdf_file = html_name.split("/")[7].split('.')[0]
+html_name = prefix_out + tfce_dir + f'output_tfce/pic_compose_combine_planar_{legend[0]}_vs_{legend[1]}_all.html'
+pdf_file = html_name.split("/")[-1].split('.')[0]
 print('pdf_file', pdf_file)
 print('%s' % html_name)
-pdfkit.from_file('%s' % html_name, '/home/sasha/MEG/GITHUB/MEG_probability/output/Norisk_vs_Risk/all_pdf/%s.pdf' % pdf_file, configuration = config, options=options)
+pdfkit.from_file('%s' % html_name, prefix_out + tfce_dir + f'output_tfce/{legend[0]}_vs_{legend[1]}/all_pdf/%s.pdf' % pdf_file, configuration = config, options=options)
 print('\tAll printed')
                         
                                                           
