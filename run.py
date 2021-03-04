5add9d43458f8de337174b3929649a90b054c3ba
asmyasikova83 <alexdsv@yandex.ru>
2021-03-04 17:31:21 +0300

    run dev

--- run.py
+++ run.py
@@ -11,7 +11,9 @@ from make_pdf_from_pic_and_html_time_course import make_pdf
 from plot_topo_stat_call import topo_stat
 from plot_topo_fdr_pdf import make_fdr_pdf
 
-def env(path):
+def env(path, prev_path=None):
+    if prev_path:
+        assert os.path.exists(prev_path) and os.path.isdir(prev_path), f"{prev_path} does not exist"
     if os.path.exists(path) and os.path.isdir(path):
         shutil.rmtree(path)
     os.makedirs(path, exist_ok = True)
@@ -67,40 +69,40 @@ elif stage == 'make_fdr_pdf':
 
 conf = conf(mode = 'grand_average', kind = ['norisk', 'risk'])
 
+path_events = prefix_out + events_dir
 if run_events_extraction:
-    path_events = prefix_out + events_dir
     env(path_events)
     subprocess.call("python risk_norisk_events_extraction.py", shell=True)
 
+path_mio = prefix_out + mio_dir
 if run_mio_correction:
-    path_mio = prefix_out + mio_dir
-    env(path_mio)
+    env(path_mio, path_events)
     mio_correction(conf)
 
+path_GA = prefix_out + GA_dir
 if run_grand_average:
-    path_GA = prefix_out + GA_dir
-    env(path_GA)
+    env(path_GA, path_mio)
     grand_average_process(conf)
 
 
+path_tfce = prefix_out + tfce_dir + GA_dir
 if run_tfce:
-    path_tfce = prefix_out + tfce_dir + GA_dir
-    env(path_tfce)
+    env(path_tfce, path_GA)
     tfce_process(conf)
 
+path_pdf = prefix_out + pdf_dir + GA_dir
 if convert_pdf:
-    path_pdf = prefix_out + pdf_dir + GA_dir
-    env(path_pdf)
+    env(path_pdf, path_tfce)
     make_pdf(conf)
 
 
+path_fdr = prefix_out + fdr_dir + GA_dir
 if run_fdr:
-    path_fdr = prefix_out + fdr_dir + GA_dir
-    env(path_fdr)
+    env(path_fdr, path_GA)
     topo_stat(conf)
 
+path_fdr_pdf = prefix_out + fdr_pdf_dir + GA_dir
 if convert_fdr_pdf:
-    path_fdr_pdf = prefix_out + fdr_pdf_dir + GA_dir
-    env(path_fdr_pdf)
+    env(path_fdr_pdf, path_fdr)
     make_fdr_pdf(conf)
 
