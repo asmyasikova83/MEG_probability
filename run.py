e53e1c50924e3daa8120adeade03ef49da4ab2e5
asmyasikova83 <alexdsv@yandex.ru>
2021-03-05 22:14:38 +0300

    def run

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import argparse
 
-from config import *
+import config
 from risk_norisk_events_extraction import risk_norisk_events
 from mio_correction import mio_correction
 
@@ -23,114 +23,115 @@ def env(path, prev_path=None):
         shutil.rmtree(path)
     os.makedirs(path, exist_ok = True)
 
-run_events_extraction = False
-run_mio_correction = False
+def run(mode, stage=None):
+    run_events_extraction = False
+    run_mio_correction = False
 
-run_grand_average = False
-run_tfr = False
-run_container = False
+    run_grand_average = False
+    run_tfr = False
+    run_container = False
 
-run_tfce = False
-convert_pdf = False
+    run_tfce = False
+    convert_pdf = False
 
-run_fdr = False
-convert_fdr_pdf = False
+    run_fdr = False
+    convert_fdr_pdf = False
 
-parser = argparse.ArgumentParser()
-parser.add_argument('mode', choices=['ga', 'tfr'], help='available modes: ga(aka grand_average), tfr')
-parser.add_argument('-s', '--stage', \
-        choices=['events', 'mio', 'ERF', 'tfr', 'container_tfr', 'tfce', 'tfce_pdf', 'fdr', 'fdr_pdf'], \
-        help='stage of processing')
-args = parser.parse_args()
-mode = args.mode
-stage = args.stage
-
-print(mode)
-if mode == 'ga':
-    conf = conf(mode = 'grand_average', kind = ['norisk', 'risk'])
-elif mode == 'tfr':
-    conf = conf(mode = 'tfr', kind = ['norisk', 'risk'], frequency = 'theta')
-
-print(stage)
-
-if not stage:
-    print('All stages!')
-    run_events_extraction = True
-    run_mio_correction = True
-
+    print(mode)
     if mode == 'ga':
-        run_grand_average = True
+        conf = config.conf(mode = 'grand_average', kind = ['norisk', 'risk'])
     elif mode == 'tfr':
+        conf = config.conf(mode = 'tfr', kind = ['norisk', 'risk'], frequency = 'theta')
+
+    print(stage)
+    if not stage:
+        print('All stages!')
+        run_events_extraction = True
+        run_mio_correction = True
+
+        if mode == 'ga':
+            run_grand_average = True
+        elif mode == 'tfr':
+            run_tfr = True
+            run_container = True
+
+        run_tfce = True
+        convert_pdf = True
+
+        run_fdr = True
+        convert_fdr_pdf = True
+
+    elif stage == 'events':
+        run_events_extraction = True
+    elif stage == 'mio':
+        run_mio_correction = True
+
+    elif stage == 'ERF':
+        assert mode == 'ga'
+        run_grand_average = True
+    elif stage == 'tfr':
+        assert mode == 'tfr'
         run_tfr = True
+    elif stage == 'container_tfr':
+        assert mode == 'tfr'
         run_container = True
 
-    run_tfce = True
-    convert_pdf = True
+    elif stage == 'tfce':
+        run_tfce = True
+    elif stage == 'tfce_pdf':
+        convert_pdf = True
 
-    run_fdr = True
-    convert_fdr_pdf = True
+    elif stage == 'fdr':
+        run_fdr = True
+    elif stage == 'fdr_pdf':
+        convert_fdr_pdf = True
 
-elif stage == 'events':
-    run_events_extraction = True
-elif stage == 'mio':
-    run_mio_correction = True
+    if run_events_extraction:
+        env(conf.path_events)
+        risk_norisk_events(conf)
 
-elif stage == 'ERF':
-    assert mode == 'ga'
-    run_grand_average = True
-elif stage == 'tfr':
-    assert mode == 'tfr'
-    run_tfr = True
-elif stage == 'container_tfr':
-    assert mode == 'tfr'
-    run_container = True
+    if run_mio_correction:
+        env(conf.path_mio, conf.path_events)
+        mio_correction(conf)
 
-elif stage == 'tfce':
-    run_tfce = True
-elif stage == 'tfce_pdf':
-    convert_pdf = True
-
-elif stage == 'fdr':
-    run_fdr = True
-elif stage == 'fdr_pdf':
-    convert_fdr_pdf = True
-
-if run_events_extraction:
-    env(conf.path_events)
-    #subprocess.call("python risk_norisk_events_extraction.py", shell=True)
-    risk_norisk_events(conf)
-
-if run_mio_correction:
-    env(conf.path_mio, conf.path_events)
-    mio_correction(conf)
-
-if mode == 'ga':
-    if run_grand_average:
-        env(conf.path_GA, conf.path_mio)
-        grand_average_process(conf)
-else:
-    if run_tfr:
-        env(conf.path_tfr, conf.path_mio)
-        tfr_process(conf)
-    if run_container:
-        env(conf.path_container, conf.path_tfr)
-        container_process(conf)
+    if mode == 'ga':
+        if run_grand_average:
+            env(conf.path_GA, conf.path_mio)
+            grand_average_process(conf)
+    else:
+        if run_tfr:
+            env(conf.path_tfr, conf.path_mio)
+            tfr_process(conf)
+        if run_container:
+            env(conf.path_container, conf.path_tfr)
+            container_process(conf)
 
 
-if run_tfce:
-    env(conf.path_tfce, conf.path_GA if mode == 'ga' else conf.path_container)
-    tfce_process(conf)
+    if run_tfce:
+        env(conf.path_tfce, conf.path_GA if mode == 'ga' else conf.path_container)
+        tfce_process(conf)
 
-if convert_pdf:
-    env(conf.path_pdf, conf.path_tfce)
-    make_pdf(conf)
+    if convert_pdf:
+        env(conf.path_pdf, conf.path_tfce)
+        make_pdf(conf)
 
 
-if run_fdr:
-    env(conf.path_fdr, conf.path_GA if mode == 'ga' else conf.path_container)
-    topo_stat(conf)
+    if run_fdr:
+        env(conf.path_fdr, conf.path_GA if mode == 'ga' else conf.path_container)
+        topo_stat(conf)
 
-if convert_fdr_pdf:
-    env(conf.path_fdr_pdf, conf.path_fdr)
-    make_fdr_pdf(conf)
+    if convert_fdr_pdf:
+        env(conf.path_fdr_pdf, conf.path_fdr)
+        make_fdr_pdf(conf)
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode', choices=['ga', 'tfr'], help='available modes: ga(aka grand_average), tfr')
+    parser.add_argument('-s', '--stage', \
+        choices=['events', 'mio', 'ERF', 'tfr', 'container_tfr', 'tfce', 'tfce_pdf', 'fdr', 'fdr_pdf'], \
+        help='stage of processing')
+    args = parser.parse_args()
+    mode = args.mode
+    stage = args.stage
+
+    run(mode, stage)
