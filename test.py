9ae3e1cb26146b8f1852ea7a99d39bb97f53ca3d
asmyasikova83 <alexdsv@yandex.ru>
2021-03-13 22:39:20 +0300

    check_ga, scheck_tfr

--- test.py
+++ test.py
@@ -19,11 +19,17 @@ def check(out_file, ref_file):
         res = False
     return res
 
-def check_ga_tstat(out_path, ref_path):
-    f = 'TFCE/GA/t_stat_norisk_vs_risk.txt'
+def check_tstat(out_path, ref_path, tstat_dir):
+    f = f'TFCE/{tstat_dir}/t_stat_norisk_vs_risk.txt'
     out_file = out_path + f
     ref_file = ref_path + f
     return check(out_file, ref_file)
+
+def check_ga_tstat(out_path, ref_path):
+    return check_tstat(out_path, ref_path, 'GA')
+
+def check_tfr_tstat(out_path, ref_path):
+    return check_tstat(out_path, ref_path, 'TFR')
 
 def check_events(out_path, ref_path):
     res = True
@@ -42,6 +48,9 @@ def check_events(out_path, ref_path):
             return False
     return res
 
+def check_tfr(out_path, ref_path):
+    return check_events(out_path, ref_path) and check_tfr_tstat(out_path, ref_path)
+
 def test(test_name, mode, stages, subjects, runs, check_func):
     print('Run test:', test_name)
     cur_dir = os.getcwd()
@@ -56,4 +65,5 @@ def test(test_name, mode, stages, subjects, runs, check_func):
 
 test('test1_events', 'ga', ['events'], ['P045','P049','P062'], ['1','3'], check_events)
 test('test2_tstat', 'ga', ['events', 'mio', 'ERF', 'tfce'], ['P045','P049','P062'], ['1','3'], check_ga_tstat)
- 
+test('test3_TFR', 'tfr', ['events', 'mio', 'tfr', 'container_tfr', 'tfce'], ['P045', 'P062'], ['1','3'], check_tfr)
+
