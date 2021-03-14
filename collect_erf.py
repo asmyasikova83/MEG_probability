c2d4efdb2f7dbdba68ebde1a92bc30dc790fb6f2
asmyasikova83 <alexdsv@yandex.ru>
2021-03-15 00:06:29 +0300

    collect_erf.py collect_tfr.py

---
+++ collect_erf.py
@@ -0,0 +1,4 @@
+from run import run
+
+run('ga',  None, [], [], work_dir='COLLECT/', test_prefix='ERF', add_date=True)
+
---
+++ collect_tfr.py
@@ -0,0 +1,4 @@
+from run import run
+
+run('tfr', None, [], [], work_dir='COLLECT/', test_prefix='TFR', add_date=True)
+
