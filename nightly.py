50f0fe4d02d25f30bea8338432b5014353cadc5b
asmyasikova83 <alexdsv@yandex.ru>
2021-03-17 06:56:35 +0300

    stages in nightly

--- nightly.py
+++ nightly.py
@@ -6,10 +6,14 @@ if __name__ == '__main__':
     parser.add_argument('name', choices=['full_ERF', 'full_TFR'], help='long tests: ERF and TFR')
     args = parser.parse_args()
     name = args.name
+    stages = ['events', 'mio']
     if name == 'full_ERF':
         mode = 'ga'
+        stages += ['ERF']
         check_func = check_ga
     elif name == 'full_TFR':
         mode = 'tfr'
+        stages += ['TFR', 'contaner_tfr']
         check_func = check_tfr
-    test(name, mode, None, [], [], check_func)
+    stages += ['tfce']
+    test(name, mode, stages, [], [], check_func)
