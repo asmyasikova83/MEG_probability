9cc5f8c9389c1dc98348bd979438b1dfd59a8ece
asmyasikova83 <asmyasnikova83@gmail.com>
2022-03-15 10:52:54 +0300

    updates in manual template

--- plotting/topo_ch_template.py
+++ plotting/topo_ch_template.py
@@ -12,19 +12,22 @@ template_for_channels = False
 anterior_cluster = False
 occipital_cluster = False
 decision = False
+post_response = False
+post_response_raw = False
+post_feedback_early = False
 post_error = False
-whole = True
+whole = False
 early_fb_cluster_trial = False
 early_fb_cluster_interaction = False
 by_one_fr = False
 by_one = False
 wide_decision = False
 super_decision  = False
-super_feedback = False
+super_feedback = True
 
 n = 1 # количество говов в ряду
 #set time for the pic  plotting
-t = 1.8 
+t = 1.7
 #time_to_plot = np.linspace(-0.8, 2.4, num = 17)
 time_to_plot = np.linspace(t, t + 0.1, num = n)
 # загружаем донора (любой Evoked с комбинированными планарами или одним планаром - чтобы было 102 сеносора). 
@@ -110,7 +113,7 @@ if occipital_cluster:
     title = 'Occipit cluster'
 if anterior_cluster:
     cluster[5,:] = 1
-    cluster[6,:] = 1
+    #luster[6,:] = 1
     cluster[9,:] = 1
     cluster[10,:] = 1
     cluster[11,:] = 1
@@ -118,11 +121,11 @@ if anterior_cluster:
     cluster[13,:] = 1
     cluster[15,:] = 1
     cluster[22,:] = 1
-    cluster[25,:] = 1
-    cluster[26,:] = 1
+    #cluster[25,:] = 1
+    #luster[26,:] = 1
     cluster[37,:] = 1
-    cluster[59,:] = 1
-    cluster[66,:] = 1 
+    #cluster[59,:] = 1
+    #cluster[66,:] = 1 
     title = 'Anterior cluster'
 if decision:
     cluster[5,:] = 1
@@ -171,13 +174,85 @@ if decision:
     title = 'Decision cluster'
 if post_error:
     cluster[8,:] = 1
-    cluster[9,:] = 1
+    #cluster[9,:] = 1
     cluster[16,:] = 1
-    cluster[17,:] = 1
-    cluster[18,:] = 1
+    #cluster[17,:] = 1
+    #cluster[18,:] = 1
     cluster[19,:] = 1
     
     title = 'Post-error'
+if post_feedback_early:
+    #cluster[70,:] = 1
+    #cluster[71,:] = 1
+    #cluster[73,:] = 1
+    #cluster[74,:] = 1
+    #cluster[75,:] = 1
+    #cluster[77,:] = 1
+    #cluster[86,:] = 1
+    cluster[60,:] = 1
+    cluster[70,:] = 1
+    cluster[71,:] = 1
+    #cluster[72,:] = 1
+    cluster[73,:] = 1
+    cluster[73,:] = 1
+    cluster[74,:] = 1
+    cluster[75,:] = 1
+    cluster[76,:] = 1
+    cluster[77,:] = 1
+    cluster[86,:] = 1
+    title = 'Post-fb early'
+if post_response_raw:
+
+    cluster[7,:] = 1
+    cluster[22,:] = 1
+    cluster[24,:] = 1
+    cluster[25,:] = 1
+    cluster[27,:] = 1
+    cluster[38,:] = 1
+    cluster[40,:] = 1
+    cluster[41,:] = 1
+    cluster[45,:] = 1
+    cluster[59,:] = 1
+    cluster[63,:] = 1
+    cluster[66,:] = 1
+    cluster[67,:] = 1
+    cluster[68,:] = 1
+    cluster[69,:] = 1
+    cluster[70,:] = 1
+    cluster[74,:] = 1
+    cluster[75,:] = 1
+    cluster[77,:] = 1
+    cluster[82,:] = 1
+    cluster[85,:] = 1
+    title = 'Post-resp_raw'
+if post_response:
+    #cluster[7,:] = 1
+    cluster[12,:] = 1
+    cluster[13,:] = 1
+    cluster[14,:] = 1
+    cluster[22,:] = 1
+    cluster[23,:] = 1
+    cluster[24,:] = 1
+    cluster[25,:] = 1
+    cluster[27,:] = 1
+    cluster[38,:] = 1
+    cluster[40,:] = 1
+    cluster[41,:] = 1
+    cluster[49,:] = 1
+    cluster[59,:] = 1
+    #cluster[63,:] = 1
+    cluster[66,:] = 1
+    cluster[67,:] = 1
+    cluster[68,:] = 1
+    cluster[69,:] = 1
+    cluster[70,:] = 1
+    cluster[74,:] = 1
+    cluster[75,:] = 1
+    cluster[77,:] = 1
+    cluster[82,:] = 1
+    cluster[83,:] = 1
+    cluster[85,:] = 1
+    title = 'Post-resp'
 if whole:
     cluster[5,:] = 1
     cluster[6,:] = 1
@@ -209,7 +284,7 @@ if whole:
     cluster[86,:] = 1
     cluster[92,:] = 1
     cluster[93,:] = 1
-    title = 'Sign ch' 
+    title = 'Whole FB' 
 if wide_decision:
     cluster[5,:] = 1
     cluster[6,:] = 1
@@ -260,18 +335,26 @@ if wide_decision:
     cluster[94,:] = 1
     title = 'wide decision'
 if super_decision:
-    cluster[15,:] = 1
     cluster[68,:] = 1
-    cluster[69,:] = 1
-    title = 'super decision'
+    cluster[75,:] = 1
+    cluster[76,:] = 1
+    title = 'super_decision'
 if super_feedback:
-    cluster[26,:] = 1
-    cluster[60,:] = 1
-    cluster[69,:] = 1
-    title = 'super feedback'
+    #cluster[59,:] = 1
+    #cluster[66,:] = 1
+    #cluster[69,:] = 1
+    #super posterior
+    #cluster[60,:] = 1
+    #cluster[69,:] = 1
+    #cluster[76,:] = 1
+    #super anterior
+    cluster[5,:] = 1
+    cluster[10,:] = 1
+    cluster[20,:] = 1
+    title = 'super_feedback'
      
 stat_sensors = np.array(cluster)
 
 os.makedirs('/net/server/data/Archive/prob_learn/asmyasnikova83/head_templates_channels_green/', exist_ok = True)
 fig = temp.plot_topomap(times = time_to_plot, ch_type='planar1', scalings = 1, units = 'dB', show = False, vmin = -0.9, vmax = 0.9, time_unit='s', title = title, colorbar = False, extrapolate = "local", mask = np.bool_(stat_sensors), mask_params = dict(marker='o', markerfacecolor='green', markeredgecolor='k', linewidth=0, markersize=10, markeredgewidth=2))
-fig.savefig('/net/server/data/Archive/prob_learn/asmyasnikova83/head_templates_channels_green/whole.jpeg', dpi = 900)
+fig.savefig('/net/server/data/Archive/prob_learn/asmyasnikova83/maps_signif_sensors/super_fb_anter.jpeg', dpi = 900)
