b385ced013b054d40e855fae9f03dd1a21e83827
asmyasikova83 <asmyasnikova83@gmail.com>
2022-03-22 01:40:32 +0300

    ttest for signed p-vals

--- plotting/p_val_gradient.py
+++ plotting/p_val_gradient.py
@@ -52,7 +52,7 @@ else:
 ########################### norisk vs risk ##############################
 for p in planars:
     print('data path', data_path)
-    _, _, risk_mean, norisk_mean = ttest_pair(data_path, response, subjects, fr, parameter1 = f'{cond1}', parameter2 = f'{cond2}', parameter3 = parameter3, parameter4 = parameter4, planar = p,  n = n)
+    _, p_val, risk_mean, norisk_mean = ttest_pair(data_path, response, subjects, fr, parameter1 = f'{cond1}', parameter2 = f'{cond2}', parameter3 = parameter3, parameter4 = parameter4, planar = p,  n = n)
     if parameter3 == 'negative':
         df = pd.read_csv(f'/{prefix}/beta/p_vals_Tukey_by_feedback_cur_MEG_early_log.csv')
     if parameter3 == None:
@@ -64,7 +64,6 @@ for p in planars:
 data_for_plotting = np.empty((102, 0))
 
 temp.data = risk_mean - norisk_mean
-
 #усредняем сигнал в каждом из интервалов усреднения и собираем в единый np.array (102 x n) где 102 - количество комбайнд планаров, а n - количество интервало
 # look at the power value to sort signed  p-vals
 for i in range(N):
@@ -83,11 +82,28 @@ for i in range(102):
         pval_norisk_risk = pval_s[f'norisk_{cond1}'].tolist()
     pval_in_intevals.append(pval_norisk_risk)
 
-pval_in_intevals = np.array(pval_in_intevals)
-pval_space_fdr = space_fdr(pval_in_intevals)
-pval_full_fdr =  full_fdr(pval_in_intevals)
+LMEM = False
+ttest = True
+
+if LMEM:
+    stat = 'LMEM'
+    pval_in_intevals = np.array(pval_in_intevals)
+    pval_space_fdr = space_fdr(pval_in_intevals)
+    pval_full_fdr =  full_fdr(pval_in_intevals)
+    # num of timeframes
+    t = 17
+    time_to_plot = np.linspace(t_start, t_end, num = N)
+if ttest:
+    stat = 'ttest'
+    pval_in_intevals = np.array(p_val)
+    pval_space_fdr = space_fdr(p_val)
+    pval_full_fdr =  full_fdr(p_val)
+    #num of timepoints
+    t = 1350
+    time_to_plot = np.arange(t_start, t_end, 0.2)
+
 # do the fdr correction of p-vals
-ozeros = np.zeros((102,17))
+ozeros = np.zeros((102,1350))
 nofdr = True
 spacefdr = False
 fullfdr = False
@@ -95,11 +111,13 @@ fullfdr = False
 if nofdr:
     types = 'no'
     for i in range(102):
-        for j in range(17):
+        for j in range((t)):
             if pval_in_intevals[i, j] < 0.05:
-                if data_for_plotting[i, j] > 0:
+                if temp.data[i, j] > 0:
+                #if data_for_plotting[i, j] > 0:
                     ozeros[i, j] = 1.0 -  10*pval_in_intevals[i, j]
-                if data_for_plotting[i, j] < 0:
+                #if data_for_plotting[i, j] < 0:
+                if temp.data[i, j] < 0:
                     ozeros[i, j] = -1.0 +  10*pval_in_intevals[i, j]
     binary_full = p_val_binary(pval_in_intevals, treshold = 0.05)
     if parameter3 == 'negative':
@@ -109,11 +127,13 @@ if nofdr:
 if spacefdr:
     types = 'space'
     for i in range(102):
-        for j in range((17)):
+        for j in range((t)):
             if pval_space_fdr[i, j] < 0.05:
-                if data_for_plotting[i, j] > 0:
+                #if data_for_plotting[i, j] > 0:
+                if temp.data[i, j] > 0:
                     ozeros[i, j] = 1.0 -  10*pval_space_fdr[i, j]
-                if data_for_plotting[i, j] < 0:
+                #if data_for_plotting[i, j] < 0:
+                if temp.data[i, j] < 0:
                     ozeros[i, j] = -1.0 +  10*pval_space_fdr[i, j]
     binary_full = p_val_binary(pval_space_fdr, treshold = 0.05)
     if parameter3 == 'negative':
@@ -123,11 +143,13 @@ if spacefdr:
 if fullfdr:
     types = 'full'
     for i in range(102):
-        for j in range((17)):
+        for j in range((t)):
             if pval_full_fdr[i, j] < 0.05:
-                if data_for_plotting[i, j] > 0:
+                #if data_for_plotting[i, j] > 0:
+                if temp.data[i, j] > 0:
                     ozeros[i, j] = 1.0 -  10*pval_full_fdr[i, j]
-                if data_for_plotting[i, j] < 0:
+                #if data_for_plotting[i, j] < 0:
+                if temp.data[i, j] < 0:
                     ozeros[i, j] = -1.0 +  10*pval_full_fdr[i, j]
     binary_full = p_val_binary(pval_full_fdr, treshold = 0.05)
     if parameter3 == 'negative':
@@ -138,16 +160,20 @@ if fullfdr:
 #prepare the p-vals for plotting
 temp.data = ozeros
 plotting_LMEM = mne.EvokedArray(temp.data, info = temp.info)
-plotting_LMEM.times = times
-time_to_plot = np.linspace(t_start, t_end, num = N)
 #TODO all figs
-fig3 = plotting_LMEM.plot_topomap(times = time_to_plot, ch_type='planar1', scalings = 1, units = 'p-val', show = False, vmin = -1.0, vmax = 1.0, time_unit='s', title = title, colorbar = True, extrapolate = "local", mask = np.bool_(binary_full), mask_params = dict(marker='o', markerfacecolor='black', markeredgecolor='yellow', linewidth=0, markersize=9, markeredgewidth=2))
+
+if LMEM:
+    plotting_LMEM.times = times
+    fig3 = plotting_LMEM.plot_topomap(times = time_to_plot, ch_type='planar1', scalings = 1, units = 'p-val', show = False, vmin = -1.0, vmax = 1.0, time_unit='s', title = title, colorbar = True, extrapolate = "local", mask = np.bool_(binary_full), mask_params = dict(marker='o', markerfacecolor='black', markeredgecolor='yellow', linewidth=0, markersize=9, markeredgewidth=2))
+if ttest:
+    plotting_LMEM.times = np.arange(-0.820, 2.420, 0.0024)
+    fig3 = plotting_LMEM.plot_topomap(times = time_to_plot, average = 0.1, scalings = 1, ch_type='planar1', units = 'p-val', show = False, vmin = -1.0, vmax = 1.0, time_unit='s', title = title, colorbar = True, extrapolate = "local", mask = np.bool_(binary_full), mask_params = dict(marker='o', markerfacecolor='black', markeredgecolor='yellow', linewidth=0, markersize=9, markeredgewidth=2))
 
 if parameter3 == 'negative':
-     os.makedirs(f'/{prefix}/topomaps/topomaps_rows_LMEM_response/p_val_sign/{cond1}/', exist_ok = True)
-     fig3.savefig(f'/{prefix}/topomaps/topomaps_rows_LMEM_response/p_val_sign/{cond1}/LMEM_{cond1}_vs_{parameter3}_vs_{parameter4}_stat_{types}_fdr_{fr}.jpeg', dpi = 300)
+     os.makedirs(f'/{prefix}/topomaps/topomaps_rows_LMEM_response/p_val_sign_ttest_ed/{cond1}/', exist_ok = True)
+     fig3.savefig(f'/{prefix}/topomaps/topomaps_rows_LMEM_response/p_val_sign_ttest_ed/{cond1}/{stat}_{cond1}_vs_{parameter3}_vs_{parameter4}_stat_{types}_fdr_{fr}.jpeg', dpi = 300)
 if parameter3 == None:
     if response:
-        os.makedirs(f'/{prefix}/topomaps/topomaps_rows_LMEM_response/p_val_sign/{cond1}_vs_{cond2}/' , exist_ok = True)
-        fig3.savefig(f'/{prefix}/topomaps/topomaps_rows_LMEM_response/p_val_sign/{cond1}_vs_{cond2}/LMEM_{cond1}_vs_{cond2}_stat_{types}_fdr_{fr}.jpeg', dpi = 300)
+        os.makedirs(f'/{prefix}/topomaps/topomaps_rows_LMEM_response/p_val_sign_ttest_ed/{cond1}_vs_{cond2}/' , exist_ok = True)
+        fig3.savefig(f'/{prefix}/topomaps/topomaps_rows_LMEM_response/p_val_sign_ttest_ed/{cond1}_vs_{cond2}/{stat}_{cond1}_vs_{cond2}_stat_{types}_fdr_{fr}.jpeg', dpi = 300)
         print('Saved!')
