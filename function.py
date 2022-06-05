9bc2a9bd522301234bff056a442caab3c181fff9
asmyasikova83 <asmyasnikova83@gmail.com>
2022-06-05 22:46:17 +0300

    customized make_beta_signal

--- function.py
+++ function.py
@@ -89,6 +89,80 @@ def fixation_cross_events(data_path_raw, data_path,  raw_name, data_path_events,
 
 ###################################################################################################################
 # функция получения эпох с выделенными частотами с применение раннего логарифмирования, сразу после сложения частот
+def make_beta_signal_events(events, events_response, subj, r, cond, fb, data_path, L_freq, H_freq, f_step, period_start, period_end, baseline, n_cycles, time_bandwidth = 4):
+    freqs = np.arange(L_freq, H_freq, f_step)
+
+    raw_fname = op.join(data_path, '{0}/run{1}_{0}_raw_ica.fif'.format(subj, r))
+
+    raw_data = mne.io.Raw(raw_fname, preload=True)
+        
+    
+    picks = mne.pick_types(raw_data.info, meg = True, eog = True)
+	   	    
+    #epochs for baseline
+    # baseline = None, чтобы не вычитался дефолтный бейзлайн
+    epochs = mne.Epochs(raw_data, events, event_id = None, tmin = -1.0, tmax = 1.0, baseline = None, picks = picks, preload = True)
+    epochs.resample(300) #resample in accordance with ti+ime window of multitaper (500 ms)
+    
+        
+    freq_show_baseline = mne.time_frequency.tfr_multitaper(epochs, freqs = freqs, n_cycles = n_cycles, time_bandwidth = time_bandwidth, use_fft = False, return_itc = False, average=False).crop(tmin=baseline[0], tmax=baseline[1], include_tmax=True) #frequency of baselin
+    #add up all values according to the frequency axis
+    b_line = freq_show_baseline.data.sum(axis=-2)
+    
+    # логарифмируем данные для бейзлайн и получаем бейзлайн в дБ
+    b_line = 10*np.log10(b_line)
+	    
+	# Для бейзлайна меняем оси местами, на первом месте число каналов
+    b_line = np.swapaxes(b_line, 0, 1)
+        
+    # выстраиваем в ряд бейзлайны для каждого из эвентов, как будто они происходили один за другим
+    a, b, c = b_line.shape
+    b_line_ave = b_line.reshape(a, b * c)
+    # Усредняем бейзлайн по всем точкам, получаем одно число (которое будем вычитать из data для каждого канала) (то же самое если вместо смены осей усреднить сначала по времени, а потом по эпохам)
+	                        
+    b = b_line_ave.mean(axis=-1)
+    b_line_new_shape = b[:, np.newaxis, np.newaxis]  
+    
+
+	####### ДЛЯ ДАННЫХ ##############
+    # baseline = None, чтобы не вычитался дефолтный бейзлайн
+    epochs = mne.Epochs(raw_data, events_response, event_id = None, tmin = period_start, 
+		                tmax = period_end, baseline = None, picks = picks, preload = True)
+		       
+    epochs.resample(300) 
+
+    freq_show = mne.time_frequency.tfr_multitaper(epochs, freqs = freqs, n_cycles = n_cycles, time_bandwidth = time_bandwidth, use_fft = False, return_itc = False, average=False)
+    temp = freq_show.data.sum(axis=2)
+    # логарифмируем данные и получаем данные в дБ
+    temp = 10*np.log10(temp)
+	    
+	####### Для данных так же меняем оси местами
+    data = np.swapaxes(temp, 0, 1)
+    data = np.swapaxes(data, 1, 2)
+	
+	
+    #Вычитаем из данных в дБ бейзлайн в дБ
+    data_dB = data - b_line_new_shape 
+    
+    # меняем оси обратно   
+    data_dB = np.swapaxes(data_dB, 1, 2)
+    data_dB = np.swapaxes(data_dB, 0, 1)
+    
+            
+    freq_show.data = data_dB[:, :, np.newaxis, :]
+        
+    #freq_show.data = freq_show.data[:, :, np.newaxis, :]
+        
+    #33 is an arbitrary number. We have to set some frequency if we want to save the file
+    freq_show.freqs = np.array([33])
+        
+    #getting rid of the frequency axis	
+    freq_show.data = freq_show.data.mean(axis=2) 
+        
+    epochs_tfr = mne.EpochsArray(freq_show.data, freq_show.info, tmin = period_start, events = events_response)
+        
+    return (epochs_tfr)
+
 def make_beta_signal(subj, r, cond, fb, data_path, L_freq, H_freq, f_step, period_start, period_end, baseline, n_cycles, time_bandwidth = 4):
     freqs = np.arange(L_freq, H_freq, f_step)
     
@@ -98,35 +172,51 @@ def make_beta_signal(subj, r, cond, fb, data_path, L_freq, H_freq, f_step, perio
 	
     #events_pos = np.loadtxt("/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/fix_cross_mio_corr/{0}_run{1}_norisk_fb_cur_positive_fix_cross.txt".format(subj, r), dtype='int') 
     events_pos = np.loadtxt("/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/fix_cross_mio_corr/{0}_run{1}_norisk_fb_cur_positive_fix_cross.txt".format(subj, r), dtype='int') 
-
+    #events_pos = np.loadtxt("/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/fix_cross_mio_corr/{0}_run{1}_norisk_fb_cur_positive_fix_cross.txt".format(subj, r), dtype='int')
         # если только одна метка, т.е. одна эпоха, то выдается ошибка, поэтому приводим shape к виду (N,3)
     if events_pos.shape == (3,):
         events_pos = events_pos.reshape(1,3)
-        
-    # download marks of negative feedback      
+    # download marks of negative feedback
+    print("/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/fix_cross_mio_corr/{0}_run{1}_norisk_fb_cur_positive_fix_cross.txt".format(subj, r))
+    print('events_pos', events_pos)
     
     #events_neg = np.loadtxt("/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/fix_cross_mio_corr/{0}_run{1}_norisk_fb_cur_negative_fix_cross.txt".format(subj, r), dtype='int')
     events_neg = np.loadtxt("/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/fix_cross_mio_corr/{0}_run{1}_norisk_fb_cur_negative_fix_cross.txt".format(subj, r), dtype='int')
+    #events_neg = np.loadtxt("/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/fix_cross_mio_corr/{0}_run{1}_norisk_fb_cur_negative_fix_cross.txt".format(subj, r), dtype='int')
     
     # если только одна метка, т.е. одна эпоха, то выдается ошибка, поэтому приводим shape к виду (N,3)
     if events_neg.shape == (3,):
         events_neg = events_neg.reshape(1,3) 
-    
-    #объединяем негативные и позитивные фидбеки для получения общего бейзлайна по ним, и сортируем массив, чтобы времена меток шли в порядке возрастания    
-    events = np.vstack([events_pos, events_neg])
+    #объединяем негативные и позитивные фидбеки для получения общего бейзлайна по ним, и сортируем массив, чтобы времена меток шли в порядке возрастания
+    #hack to pick more data
+    print('events_neg', events_neg)
+    if events_pos.size != 0 and events_neg.size != 0:
+        print('vstacking..')
+        events = np.vstack([events_pos, events_neg])
+    elif events_pos.size == 0 and events_neg.size != 0:
+        events = events_neg
+    elif events_pos.size != 0 and events_neg.size == 0:
+        events = events_pos
+    else:
+        pass
     events = np.sort(events, axis = 0) 
-       #remove doubles with the same timing
-    events = np.unique(events, axis = 0) 
+    print(events)
+    #remove doubles with the same timing
+    events = np.unique(events, axis = 0)
+    print(events)
     #events, which we need
-    #events_response = np.loadtxt('/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/events_by_cond_mio_corrected/{0}_run{1}_{2}_fb_cur_{3}.txt'.format(subj, r, cond, fb), dtype='int')
+    #vents_response = np.loadtxt('/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/events_by_cond_mio_corrected/{0}_run{1}_{2}_fb_cur_{3}.txt'.format(subj, r, cond, fb), dtype='int')
+    #print('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_mio_list_compare/{0}_run{1}_{2}_fb_{3}.txt')
+    print(subj, r, cond, fb)
     events_response = np.loadtxt('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_mio_list_compare/{0}_run{1}_{2}_fb_{3}.txt'.format(subj, r, cond, fb), dtype='int')
+    #events_response = np.loadtxt('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_mio_list_compare/{0}_run{1}_{2}_fb_{3}.txt'.format(subj, r, cond, fb), dtype='int')
     
     # если только одна метка, т.е. одна эпоха, то выдается ошибка, поэтому приводи shape к виду (N,3)
     if events_response.shape == (3,):
         events_response = events_response.reshape(1,3)
+    '''
     counter = 0
     short_ev_resp = []
-    '''
     for e in events_response:
         short_ev_resp.append(e)
         counter = counter + 1
@@ -136,6 +226,7 @@ def make_beta_signal(subj, r, cond, fb, data_path, L_freq, H_freq, f_step, perio
             pass
     events_response = short_ev_resp
     '''
+    #print('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_mio_list_compare/{0}_run{1}_{2}_fb_{3}.txt'.format(subj, r, cond, fb))
     print('events_resp', events_response)
     raw_fname = op.join(data_path, '{0}/run{1}_{0}_raw_ica.fif'.format(subj, r))
 
