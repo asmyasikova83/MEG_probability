2fabe5df7d4a1ce4397e73784a1eb765a64b9378
asmyasikova83 <asmyasnikova83@gmail.com>
2022-06-05 23:05:19 +0300

    refactor

--- plotting/plot_topomaps_vs_zero.py
+++ plotting/plot_topomaps_vs_zero.py
@@ -8,88 +8,150 @@ import numpy as np
 from scipy import stats
 import copy
 import statsmodels.stats.multitest as mul
-from function import ttest_pair, ttest_vs_zero, space_fdr, full_fdr, p_val_binary, plot_deff_topo, plot_topo_vs_zero
+from function import ttest_pair, ttest_pair_independent, ttest_vs_zero, space_fdr, full_fdr, p_val_binary, plot_deff_topo, plot_topo_vs_zero
 from config import *
 
 ###################### при построении topomaps берем только тех испытуемых, у которых есть все категории условий  see config ###################
-#data_path = '/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/beta_16_30_ave_into_subjects_comb_planar'
+
+
+freq_range =  'beta_16_30_trf_early_log'
+fr1 =  'beta_16_30_trf_early_log'
+fr2 =  'beta_16_30_trf_early_log'
+
+#data path
 if not response:
+    #stimulus data
     data_path = '/net/server/data/Archive/prob_learn/asmyasnikova83/stim_check/beta_16_30_trf_no_log_division_stim/beta_16_30_trf_no_log_division_stim_ave_comb_planar/'
     path = '/net/server/data/Archive/prob_learn/asmyasnikova83/beta/topomaps_stim/ttests/'
 else:
     if parameter3 == 'negative':
-        data_path = '/net/server/data/Archive/prob_learn/asmyasnikova83/beta_by_feedback/{0}_ave_into_subj_comb_planar/'.format(freq_range)
+        if Normals:
+            #data_path = '/net/server/data/Archive/prob_learn/asmyasnikova83/beta_by_feedback/{0}_ave_into_subj_comb_planar/'.format(freq_range)
+            data_path = '/net/server/data/Archive/prob_learn/asmyasnikova83/beta_by_feedback/beta_16_30_trf_early_log_ave_into_subjects_comb_planar/'.format(freq_range)
+        if Autists:
+            data_path = '/net/server/data/Archive/prob_learn/asmyasnikova83/Autists/beta_by_feedback/{0}_ave_into_subjects_comb_planar/'.format(freq_range)
+        if Normals_Autists:
+            #contrast between N and A
+            data_path1 = '/net/server/data/Archive/prob_learn/asmyasnikova83/Autists/beta_by_feedback/{0}_ave_into_subjects_comb_planar/'.format(freq_range)
+            #data_path2 = '/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/beta_16_30_ave_into_subjects_comb_planar'
+            data_path2 = '/net/server/data/Archive/prob_learn/asmyasnikova83/beta_by_feedback/{0}_ave_into_subjects_comb_planar/'.format(freq_range)
     if  parameter3 == None:
-        data_path = '/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/beta_16_30_ave_into_subjects_comb_planar'
+        if Normals:
+            data_path = '/net/server/data/Archive/prob_learn/asmyasnikova83/beta/beta_16_30_ave_into_subjects_comb_planar/'
+        if Autists:
+            data_path = '/net/server/data/Archive/prob_learn/asmyasnikova83/Autists/{0}/{0}_ave_into_subj_comb_planar/'.format(freq_range)
+        if Normals_Autists:
+            #contrast between N and A
+            data_path1 = '/net/server/data/Archive/prob_learn/asmyasnikova83/Autists/{0}/{0}_ave_into_subj_comb_planar/'.format(freq_range)
+            #data_path2 = '/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/beta_16_30_ave_into_subjects_comb_planar'
+            data_path2 = '/net/server/data/Archive/prob_learn/asmyasnikova83/beta/beta_16_30_ave_into_subjects_comb_planar/'
 
-path = '/net/server/data/Archive/prob_learn/asmyasnikova83/supplementary/ttests/'
-freq_range = 'beta_16_30_trf_early_log'
+#path for plots
+if Normals:
+    path = '/net/server/data/Archive/prob_learn/asmyasnikova83/supplementary/Normals/ttests_fin_ch_bline/'
+if Autists:
+    path = '/net/server/data/Archive/prob_learn/asmyasnikova83/supplementary/Autists/ttests_fin_ch_bline/'
+if Normals_Autists:
+    print(path)
+    path = '/net/server/data/Archive/prob_learn/asmyasnikova83/supplementary/Normals_Autists/ttests_fin_ch_bline/'
+print(path)
 os.makedirs(path, exist_ok = True)
 # задаем время и донора
 time_to_plot = np.linspace(-0.8, 2.4, num = 17)
 temp = mne.Evoked("/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/beta_16_30_ave_into_subjects_comb_planar/P001_norisk_evoked_beta_16_30_resp_comb_planar.fif")
 
 n = temp.data.shape[1] # количество временных отчетов для combined planars - temp.data.shape = (102 x n), где 102 - количество планаров, а n - число временных отчето
+#resolution of plots, settings for plotting
 resolution = 300
-planars = 'comb_planar'
-######### контраст norisk losses vs 0, without correction #########################
-t_stat_risk, p_val_risk, risk_mean = ttest_vs_zero(data_path, subjects, parameter1 = 'norisk', parameter3 = 'negative', freq_range = freq_range, planar = planars, n = n)
+vmin = -4.5
+vmax = 4.5
+vmin_contrast = -1.2
+vmax_contrast = 1.2
+choice_type_vs_zero = False
+main_contrast = False
+main_contrast_across_samples = True
+#check the sample of participants and data path
+print(data_path)
+#print(subjects)
 
-fig, temp = plot_topo_vs_zero(p_val_risk, temp, risk_mean, time_to_plot, title = 'norisk losses no FDR')
+#choice_type = ['norisk', 'risk', 'prerisk', 'postrisk']
+choice_type = ['prerisk']
+#for risk fb neg, risk fb pos 21 autists (minus 322, 326, 327)
+#for prerisk fb neg, risk fb pos 20 autists (minus 313, 318, 333, 312, 316, 322, 326,  P329) - 13 subj
+######### контраст choice_type vs 0, without correction #########################
+for choice in choice_type:
+    if choice_type_vs_zero:
+        assert(not Normals_Autists)
 
-fig.savefig(path + 'norisk_losses_vs_0.jpeg', dpi = resolution)
-    ######### контраст norisk wins vs 0, without correction #########################
-t_stat_risk, p_val_risk, risk_mean = ttest_vs_zero(data_path, subjects, parameter1 = 'norisk', parameter3 = 'positive', freq_range = freq_range, planar = planars, n = n)
+        if parameter3 == None:
+            t_stat_risk, p_val_risk, risk_mean = ttest_vs_zero(data_path, subjects, parameter1 = choice, parameter3 = parameter3, freq_range = freq_range, planar = planars, n = n)
 
-fig, temp = plot_topo_vs_zero(p_val_risk, temp, risk_mean, time_to_plot, title = 'norisk wins no FDR')
+            fig, temp = plot_topo_vs_zero(p_val_risk, temp, risk_mean, time_to_plot, vmin, vmax, title = f'{choice} {sample} no fdr')
 
-fig.savefig(path + 'norisk_wins_vs_0.jpeg', dpi = resolution)
-######### контраст prerisk losses vs 0, without correction #########################
-t_stat_risk, p_val_risk, risk_mean = ttest_vs_zero(data_path, subjects, parameter1 = 'prerisk', parameter3 = 'negative', freq_range = freq_range, planar = planars, n = n)
+            fig.savefig(path + f'{choice}_vs_0_{sample}_test.jpeg', dpi = resolution)
 
-fig, temp = plot_topo_vs_zero(p_val_risk, temp, risk_mean, time_to_plot, title = 'prerisk losses no FDR')
+        if parameter3 == 'negative':
+            t_stat_risk, p_val_risk, risk_mean = ttest_vs_zero(data_path, subjects, parameter1 = choice, parameter3 = parameter3, freq_range = freq_range, planar = planars, n = n)
 
-fig.savefig(path + 'prerisk_losses_vs_0.jpeg', dpi = resolution)
-######### контраст prerisk wins vs 0, without correction #########################
-t_stat_risk, p_val_risk, risk_mean = ttest_vs_zero(data_path, subjects, parameter1 = 'prerisk', parameter3 = 'positive', freq_range = freq_range, planar = planars, n = n)
+            fig, temp = plot_topo_vs_zero(p_val_risk, temp, risk_mean, time_to_plot, vmin, vmax, title = f'{choice} {parameter3} {sample}')
 
-fig, temp = plot_topo_vs_zero(p_val_risk, temp, risk_mean, time_to_plot, title = 'prerisk wins no FDR')
+            fig.savefig(path + f'{choice}_{parameter3}_vs_0_{sample}.jpeg', dpi = resolution)
 
-fig.savefig(path + 'prerisk_wins_vs_0.jpeg', dpi = resolution)
-######### контраст risk losses vs 0, without correction #########################
-t_stat_risk, p_val_risk, risk_mean = ttest_vs_zero(data_path, subjects, parameter1 = 'risk', parameter3 = 'negative', freq_range = freq_range, planar = planars, n = n)
+            t_stat_risk, p_val_risk, risk_mean = ttest_vs_zero(data_path, subjects, parameter1 = choice, parameter3 = parameter4, freq_range = freq_range, planar = planars, n = n)
 
-fig, temp = plot_topo_vs_zero(p_val_risk, temp, risk_mean, time_to_plot, title = 'risk losses no FDR')
+            fig, temp = plot_topo_vs_zero(p_val_risk, temp, risk_mean, time_to_plot, vmin, vmax, title = f'{choice} {parameter4} {sample}')
 
-fig.savefig(path + 'risk_losses_vs_0.jpeg', dpi = resolution)
-######### контраст risk wins vs 0, without correction #########################
-t_stat_risk, p_val_risk, risk_mean = ttest_vs_zero(data_path, subjects, parameter1 = 'risk', parameter3 = 'positive', freq_range = freq_range, planar = planars, n = n)
+            fig.savefig(path + f'{choice}_{parameter4}_vs_0_{sample}.jpeg', dpi = resolution)
 
-fig, temp = plot_topo_vs_zero(p_val_risk, temp, risk_mean, time_to_plot, title = 'risk wins no FDR')
+######### контраст norisk vs choice type in a sample separately, no full fdr correction #########################
 
-fig.savefig(path + 'risk_wins_vs_0.jpeg', dpi = resolution)
-######### контраст postrisk losses vs 0, without correction #########################
-t_stat_risk, p_val_risk, risk_mean = ttest_vs_zero(data_path, subjects, parameter1 = 'postrisk', parameter3 = 'negative', freq_range = freq_range, planar = planars, n = n)
+    if main_contrast:
+        assert(not Normals_Autists)
+        t_stat, p_val, risk_mean, norisk_mean = ttest_pair(data_path, response, subjects, fr = freq_range, parameter1 = choice, parameter2 = 'norisk', parameter3 = parameter3, parameter4 = parameter4 , planar = planars,  n = n)
 
-fig, temp = plot_topo_vs_zero(p_val_risk, temp, risk_mean, time_to_plot, title = 'postrisk losses no FDR')
+        p_val_no_fdr = p_val
 
-fig.savefig(path + 'postrisk_losses_vs_0.jpeg', dpi = resolution)
-######### контраст postrisk wins vs 0, without correction #########################
-	
-t_stat_risk, p_val_risk, risk_mean = ttest_vs_zero(data_path, subjects, parameter1 = 'postrisk', parameter3 = 'positive', freq_range = freq_range, planar = planars, n = n)
+        if parameter3 == None:
 
-fig, temp = plot_topo_vs_zero(p_val_risk, temp, risk_mean, time_to_plot, title = 'postrisk wins no FDR')
+            _, fig1, temp = plot_deff_topo(p_val_no_fdr, Normals_Autists, temp, risk_mean, norisk_mean, time_to_plot, vmin_contrast, vmax_contrast, title = f'{choice} minus norisk {sample}, no FDR')
 
-fig.savefig(path + 'postrisk_wins_vs_0.jpeg', dpi = resolution)
-'''
-######### контраст norisk vs postrisk, with full fdr correction #########################
+            fig1.savefig(path + f'{choice}_minus_norisk_stat_no_fdr.jpeg', dpi = resolution)
 
-t_stat, p_val, risk_mean, norisk_mean = ttest_pair(data_path, response, subjects, fr = freq_range, parameter1 = 'postrisk', parameter2 = 'norisk', parameter3 = None, parameter4 = None , planar = 'comb_planar',  n = n)
+        if parameter3 == 'negative':
 
-p_val_full_fdr = full_fdr(p_val)
+            _, fig1, temp = plot_deff_topo(p_val_no_fdr, Normals_Autists, temp, risk_mean, norisk_mean, time_to_plot, vmin_contrast, vmax_contrast, title = f'{choice} {parameter3}-{parameter4}')
 
-_, fig1, temp = plot_deff_topo(p_val_full_fdr, temp, norisk_mean, risk_mean, time_to_plot, title = 'norisk vs postrisk, full FDR')
+            fig1.savefig(path + f'{choice}_{parameter3}_minus_{parameter4}_stat_no_fdr.jpeg', dpi = resolution)
 
-fig1.savefig(path + 'norisk_vs_postrisk_stat_full_fdr.jpeg', dpi = 900)
+######### контраст norisk vs choice type across Normals and Autists, no full fdr correction #########################
+    if main_contrast_across_samples:
+        assert(Normals_Autists)
 
-'''
+        if parameter3 == None:
+            t_stat, p_val, risk_mean, norisk_mean = ttest_pair_independent(data_path1, data_path2, response, Autists, Normals, fr1, fr2, parameter1 = choice, 
+                                                                       parameter3 = parameter3, planar = planars,  n = n)
+
+            p_val_no_fdr = p_val
+
+
+            _, fig1, temp = plot_deff_topo(p_val_no_fdr,  Normals_Autists, temp, risk_mean, norisk_mean, time_to_plot, vmin_contrast, vmax_contrast, title = f'{choice} Autists minus {choice} Norm, no FDR')
+
+            fig1.savefig(path + f'{choice}_minus_{choice}_{sample}_stat_no_fdr.jpeg', dpi = resolution)
+
+        if parameter3 == 'negative':
+
+            t_stat, p_val, risk_mean, norisk_mean = ttest_pair_independent(data_path1, data_path2, response, Autists, Normals, fr1, fr2, parameter1 = choice, 
+                                                                       parameter3 = parameter3, planar = planars,  n = n)
+
+            p_val_no_fdr = p_val
+            _, fig1, temp = plot_deff_topo(p_val_no_fdr,  Normals_Autists, temp, risk_mean, norisk_mean, time_to_plot, vmin_contrast, vmax_contrast, title = f'{choice} Autists {parameter3}- Norm {parameter3}, no FDR')
+
+            fig1.savefig(path + f'{choice}_{parameter3}-{parameter3}_{sample}_stat_no_fdr.jpeg', dpi = resolution)
+
+            t_stat, p_val, risk_mean, norisk_mean = ttest_pair_independent(data_path1, data_path2, response, Autists, Normals, fr1, fr2, parameter1 = choice, 
+                                                                       parameter3 = parameter4, planar = planars,  n = n)
+
+            p_val_no_fdr = p_val
+            _, fig1, temp = plot_deff_topo(p_val_no_fdr,  Normals_Autists, temp, risk_mean, norisk_mean, time_to_plot, vmin_contrast, vmax_contrast, title = f'{choice} Autists {parameter4}-Norm {parameter4}, no FDR')
+
+            fig1.savefig(path + f'{choice}_{parameter4}-{parameter4}_{sample}_stat_no_fdr.jpeg', dpi = resolution)
