fd60f46a9439c37bb6e535b86cc4e72947739b0b
asmyasikova83 <asmyasnikova83@gmail.com>
2022-06-05 22:59:44 +0300

    refactor

--- plotting/make_pdf.py
+++ plotting/make_pdf.py
@@ -1,6 +1,6 @@
 import os
 import pdfkit
-from config import parameter3, cond1, cond2, planars, path, path_pdf
+from config import parameter3, cond1_name, cond2_name, planars, path, path_pdf
 
 
 config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
@@ -12,13 +12,14 @@ options = {
     'no-outline':None,
     'quiet':''
  }
-
+planars = ['comb_planar']
 if parameter3 == 'negative':
     name = cond1
     cond1 = name + '_negative'
-    cond2 = name + '_positive'       
+    cond2 = name + '_positive'
+    pic_compose_comb_planar_LP_vs_HP_all.html
 for planar in planars:
-    html_name =  path + 'output/' + 'pic_compose_%s_%s_vs_%s_%s.html' % (planar, f'{cond1}', f'{cond2}', 'all')
+    html_name =  path + 'output/' + 'pic_compose_%s_%s_vs_%s_%s.html' % (planar, f'{cond1_name}', f'{cond2_name}', 'all')
     pdf_file = html_name.split("/")[-1].split('.')[0]
     pdfkit.from_file('%s' % html_name, path_pdf + '%s.pdf' % pdf_file, configuration = config, options=options)
     print('\tAll printed')
