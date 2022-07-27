c62d78b8e5c6cdb8c5ed4d8681e7e166fd603749
asmyasikova83 <asmyasnikova83@gmail.com>
2022-07-27 16:06:30 +0300

    refactor

--- plotting/plot_topomaps_factors_heads.py
+++ plotting/plot_topomaps_factors_heads.py
@@ -4,10 +4,15 @@ import numpy as np
 import pandas as pd
 from function import ttest_pair, ttest_vs_zero, space_fdr, full_fdr, p_val_binary, plot_deff_topo, plot_topo_vs_zero, nocolor_topomaps_line
 from config import *
-import re
+#import re
 
 pre_fb = False
+decision = False
+post_response = False
+anticip = False
 fb = True
+early_fb = True
+late_fb = False
 
 path = '/net/server/data/Archive/prob_learn/asmyasnikova83/probability/signif_sensors/'
 os.makedirs(path, exist_ok = True)
@@ -46,25 +51,26 @@ if pre_fb:
     pval_full_fdr =  full_fdr(pval_in_intevals)
     sensors = []
     for j in range(102):
-        if pval_full_fdr[j,0] < 0.002 and pval_full_fdr[j,1] < 0.002 and pval_full_fdr[j,2] < 0.002:
-            f_name = path + 'sensors_decision_pre_resp_900_200_super_sign.txt'
-            sensors.append(j)
-        '''
-        if pval_full_fdr[j,5] < 0.05 and pval_full_fdr[j,6] < 0.05:
-            f_name = path + 'sensors_100_500_post_resp.txt'
-            #manually remove outliers TODO automatically
-            if j == 63 or j == 45 or j == 7:
-                pass
-            else:
+        if decision:
+            if pval_full_fdr[j,0] < 0.002 and pval_full_fdr[j,1] < 0.002 and pval_full_fdr[j,2] < 0.002:
+                f_name = path + 'sensors_decision_pre_resp_900_200_super_sign.txt'
                 sensors.append(j)
-        if pval_full_fdr[j,8] < 0.05:
-            f_name = path + 'sensors_fb_anticip_600_800.txt'
-            #manually rm outliers
-            if j == 48:
-                pass
-            else:
-                sensors.append(j)
-        '''
+        if post_response:
+            if pval_full_fdr[j,5] < 0.05 and pval_full_fdr[j,6] < 0.05:
+                f_name = path + 'sensors_100_500_post_resp.txt'
+                #manually remove outliers TODO automatically
+                if j == 63 or j == 45 or j == 7:
+                    pass
+                else:
+                    sensors.append(j)
+        if anticip:
+            if pval_full_fdr[j,8] < 0.05:
+                f_name = path + 'sensors_fb_anticip_600_800.txt'
+                #manually rm outliers
+                if j == 48:
+                    pass
+                else:
+                    sensors.append(j)
     sensors_to_save = np.array(sensors)
     sensors_to_save = sensors_to_save[np.newaxis]
     np.savetxt(f_name, sensors_to_save, fmt="%s")
@@ -82,20 +88,19 @@ if fb:
     pval_full_fdr =  full_fdr(pval_in_intevals)
     sensors = []
     for j in range(102):
-        '''
-        if pval_full_fdr[j,10] < 0.05 and pval_full_fdr[j,11] < 0.05:
-            f_name = path + 'sensors_early_fb_1100_1500.txt'
-            sensors.append(j)
-        '''
-        if pval_full_fdr[j, 12] < 0.02 and pval_full_fdr[j, 13] < 0.02:
-        #if pval_full_fdr[j, 13] < 0.02:
-            print(pval_full_fdr[j, 12])
-            print(pval_full_fdr[j, 13])
-            print(j)
-            f_name = path + 'sensors_late_fb_1500_1900_super_sign.txt'
-            sensors.append(j)
+        if early_fb:
+            if pval_full_fdr[j,11] < 0.05 and pval_full_fdr[j,12] < 0.05:
+                f_name = path + 'sensors_early_fb_1100_1500.txt'
+                sensors.append(j)
+        if late_fb:
+            if pval_full_fdr[j, 12] < 0.035 and pval_full_fdr[j, 13] < 0.035:
+                #TODO correct ..topomaps signif sensors
+                f_name = path + 'sensors_late_fb_1500_1900_super_sign.txt'
+                sensors.append(j)
     print(sensors)
-    exit()
-    sensors_to_save = np.array(sensors)
-    sensors_to_save = sensors_to_save[np.newaxis]
-    np.savetxt(f_name, sensors_to_save, fmt="%s")
+    if len(sensors) != 0:
+        sensors_to_save = np.array(sensors)
+        sensors_to_save = sensors_to_save[np.newaxis]
+        np.savetxt(f_name, sensors_to_save, fmt="%s")
+    else:
+        print('Empty list of signif sensors')
