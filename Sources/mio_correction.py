5dcc75b91672cda4801a1ed9445e67abe9acf99d
asmyasikova83 <asmyasnikova83@gmail.com>
2022-09-07 20:43:11 +0300

    learnung type in mio_correction.py

--- Sources/mio_correction.py
+++ Sources/mio_correction.py
@@ -2,6 +2,8 @@ import mne, os
 import os.path as op
 import numpy as np
 import pathlib
+from os.path import exists
+from find_fix_cross import read_events
 from config import *
 
 def clean_events(epochs_ar, thres):
@@ -35,12 +37,18 @@ def call_clean_events(data_path, data_path_raw, raw_name, subj, r, cond, fb):
     raw_fname = op.join(data_path, '{0}/run{1}_{0}_raw_ica.fif'.format(subj, r))
     raw_data = mne.io.Raw(raw_fname, preload=True).filter(l_freq=70, h_freq=None)
     #raw_data = mne.io.Raw(conf.fpath_raw.format(subj, run, subj), preload=True, verbose='ERROR').filter(l_freq=70, h_freq=None)
-    
+    #for po63-p067 and autists 
     events_raw = np.loadtxt(op.join(data_path_raw, raw_name.format(subj, r)), dtype = 'int')
+    #For p00-p062
+    #events_raw = read_events(op.join(data_path_raw, raw_name.format(subj, r)))
     if Autists:
-        events = np.loadtxt('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_autists/Events_autists_test/{0}_run{1}_{2}_fb_{3}.txt'.format(subj, r, cond, fb), dtype='int')
+        if exists('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_autists{0}/{1}_run{2}_{3}_fb_{4}.txt'.format(prefix, subj, r, cond, fb)):
+            events = np.loadtxt('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_autists{0}/{1}_run{2}_{3}_fb_{4}.txt'.format(prefix, subj, r, cond, fb), dtype='int')
+        else:
+            pass
     if Normals:
-        events = np.loadtxt('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_add/Events_add_subj/{0}_run{1}_{2}_fb_{3}.txt'.format(subj, r, cond, fb), dtype='int')
+        if exists('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_normals{0}/{1}_run{2}_{3}_fb_{4}.txt'.format(prefix, subj, r, cond, fb)):
+            events = np.loadtxt('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_normals{0}/{1}_run{2}_{3}_fb_{4}.txt'.format(prefix, subj, r, cond, fb), dtype='int')
     # (3,) -> (1,3)
     if events.shape == (3,):
         events = events[np.newaxis, :]
@@ -57,11 +65,13 @@ def call_clean_events(data_path, data_path_raw, raw_name, subj, r, cond, fb):
     if cleaned.any():
         print('events after', cleaned)
         if Autists:
-            mio_corrected_events_file = open('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_mio_list_compare/{0}_run{1}_{2}_fb_{3}.txt'.format(subj, r, cond, fb), "w")
+            mio_corrected_events_file = open('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_mio_list_compare{0}/{1}_run{2}_{3}_fb_{4}.txt'.format(prefix, subj, r, cond, fb), "w")
         if Normals:
-            mio_corrected_events_file = open('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_mio_list_compare/{0}_run{1}_{2}_fb_{3}.txt'.format(subj, r, cond, fb), "w")
+            mio_corrected_events_file = open('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_mio_list_compare{0}/{1}_run{2}_{3}_fb_{4}.txt'.format(prefix, subj, r, cond, fb), "w")
         np.savetxt(mio_corrected_events_file, cleaned, fmt="%d")
         mio_corrected_events_file.close()
+
+prefix = '_not_trained'
 
 rounds = [1, 2, 3, 4, 5, 6]
 trial_type = ['norisk', 'prerisk', 'risk', 'postrisk']
@@ -70,30 +80,36 @@ trial_type = ['norisk', 'prerisk', 'risk', 'postrisk']
 feedback = ['positive', 'negative']
 period_start = -1.750
 period_end = 2.750
-
 data_path = '/net/server/data/Archive/prob_learn/vtretyakova/ICA_cleaned'
 assert(not Normals_Autists)
 if Autists:
     data_path_raw = '/net/server/data/Archive/prob_learn/data_processing/Autists/mio_free_events/'
-    os.makedirs('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_mio_list_compare/', exist_ok = True)
+    os.makedirs('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_mio_list_compare{0}/'.format(prefix), exist_ok = True)
 if Normals:
+    subjects = ['P063', 'P064', 'P065', 'P066', 'P067']
     data_path_raw = '/net/server/data/Archive/prob_learn/data_processing/mio_free_events/'
-    os.makedirs('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_mio_list_compare/', exist_ok = True)
+    #data_path_raw = '/net/server/data/home/inside/Events_probability/Events_clean'
+    os.makedirs('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_mio_list_compare{0}/'.format(prefix), exist_ok = True)
+
+#for po063-p0567 and autists
 raw_name = '{0}/{0}_run{1}_mio_free.txt'
+#raw_name = '{0}_run{1}_events_clean.txt'
 
 '''
 extract cleaned events
 '''
 print('\trun mio_extraction...')
 
+print(subjects)
+
 for subj in subjects:
     for r in rounds:
         for cond in trial_type:
             for fb in feedback:
                 if Autists:
-                    rf = '/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_autists/Events_autists_test/{0}_run{1}_{2}_fb_{3}.txt'.format(subj, r, cond, fb)
+                    rf = '/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_autists{0}/{1}_run{2}_{3}_fb_{4}.txt'.format(prefix, subj, r, cond, fb)
                 if Normals:
-                    rf = '/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_add/Events_add_subj/{0}_run{1}_{2}_fb_{3}.txt'.format(subj, r, cond, fb)
+                    rf = '/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_normals{0}/{1}_run{2}_{3}_fb_{4}.txt'.format(prefix, subj, r, cond, fb)
                 if pathlib.Path(rf).exists() and os.stat(rf).st_size != 0:
                     call_clean_events(data_path, data_path_raw, raw_name, subj, r, cond, fb)
                 else:
