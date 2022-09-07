ba7c49c69feb8562e95f166ed9fd9b829509351d
asmyasikova83 <asmyasnikova83@gmail.com>
2022-09-07 20:32:35 +0300

    type of learning in fix cross

--- Sources/find_fix_cross.py
+++ Sources/find_fix_cross.py
@@ -17,16 +17,16 @@ def read_events(filename):
 #####################################################################################
 ######## Функция для поиска меток фиксационного креста (по ним ищется baseline)######
 
-def fixation_cross_events(data_path_raw, raw_name, data_path_events, name_events, subj, r, fb):
+def fixation_cross_events(data_path_raw, raw_name, data_path_events, name_events, subj, r, t, fb):
     
     # для чтения файлов с events используйте либо np.loadtxt либо read_events либо read_events_N
-    print(op.join(data_path_events, name_events.format(subj, r, fb)))
-    no_risk = np.loadtxt(op.join(data_path_events, name_events.format(subj, r, fb)), dtype='int')
-    print(no_risk)
+    print(op.join(data_path_events, name_events.format(subj, r, t, fb)))
+    no_risk = np.loadtxt(op.join(data_path_events, name_events.format(subj, r, t, fb)), dtype='int')
     #no_risk = read_events(op.join(data_path_events, name_events.format(subj, r)))
     
-    # Load raw events without miocorrection
+    # Load raw events without miocorrection for p001-p062
     #events_raw = read_events(op.join(data_path_raw, raw_name.format(subj, r)))
+    # Load raw events without miocorrection for p063-p067
     events_raw = np.loadtxt(op.join(data_path_raw, raw_name.format(subj, r)), dtype = 'int')
     # Load data
 
@@ -37,7 +37,6 @@ def fixation_cross_events(data_path_raw, raw_name, data_path_events, name_events
     #events_raw = mne.find_events(raw, stim_channel='STI101', output='onset', 
     #                                 consecutive='increasing', min_duration=0, shortest_event=1, mask=None, 
     #                                 uint_cast=False, mask_type='and', initial_event=False, verbose=None)
-    
     if no_risk.shape == (3,):
         no_risk = no_risk.reshape(1,3)
     # список индексов трайлов
--- Sources/fix_cross_main.py
+++ Sources/fix_cross_main.py
@@ -3,29 +3,39 @@ import os
 import os.path as op
 import numpy as np
 from find_fix_cross import fixation_cross_events
+from config import *
 
 rounds = [1, 2, 3, 4, 5, 6]
+#rounds = [4]
 
-
-#rial_type = ['norisk', 'prerisk', 'risk', 'postrisk']
-trial_type = ['norisk']
+trial_type = ['norisk', 'prerisk', 'risk', 'postrisk']
+#trial_type = ['risk']
 
 feedback = ['positive', 'negative']
 #set type of sample - Autists or Normals with a list of subjects- in config
 
+#prefix = '_not_trained'
+prefix = '_ignore_train'
+
 if Autists:
+    #subjects = ['P342']
     data_path_raw = '/net/server/data/Archive/prob_learn/data_processing/Autists/mio_free_events/'
     raw_name = '{0}/{0}_run{1}_mio_free.txt'
-    data_path_events = '/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_mio_list_compare/'
-    name_events = '{0}_run{1}_norisk_fb_{2}.txt' 
-    os.makedirs('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/fix_cross_mio_corr/', exist_ok = True)
+    #data_path_events = '/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_mio_list_compare/'
+    #{subject}_run{r}_{trial_type}_fb_{fb}.txt'
+    data_path_events = '/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/Events_mio_list_compare{0}/'.format(prefix)
+    name_events = '{0}_run{1}_{2}_fb_{3}.txt'
+    os.makedirs('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/fix_cross_mio_corr{0}/'.format(prefix), exist_ok = True)
 if Normals:
-    subjects = ['P063', 'P064', 'P065', 'P066']
+    #subjects = ['P063', 'P064', 'P065', 'P066', 'P067']
     data_path_raw = '/net/server/data/Archive/prob_learn/data_processing/mio_free_events/'
     raw_name = '{0}/{0}_run{1}_mio_free.txt'
-    data_path_events = '/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_mio_list_compare/'
-    name_events = '{0}_run{1}_norisk_fb_{2}.txt' 
-    os.makedirs('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/fix_cross_mio_corr/', exist_ok = True)
+    #data_path_raw = '/net/server/data/home/inside/Events_probability/Events_clean'
+    #raw_name = '{0}_run{1}_events_clean.txt'
+    #data_path_events = '/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_mio_list_compare/'
+    data_path_events = '/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/Events_mio_list_compare{0}/'.format(prefix)
+    name_events = '{0}_run{1}_{2}_fb_{3}.txt'
+    os.makedirs('/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/fix_cross_mio_corr{0}/'.format(prefix), exist_ok = True)
 
 
 for subj in subjects:
@@ -34,15 +44,14 @@ for subj in subjects:
             for fb in feedback:
                 
                 try:
-                    event_fixation_cross_norisk = fixation_cross_events(data_path_raw, raw_name, data_path_events, name_events, subj, r, fb)
+                    event_fixation_cross_norisk = fixation_cross_events(data_path_raw, raw_name, data_path_events, name_events, subj, r, t, fb)
                     if Autists:
-                        f = "/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/fix_cross_mio_corr/{0}_run{1}_{2}_fb_cur_{3}_fix_cross.txt".format(subj, r, t, fb)
-                        np.savetxt("/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/fix_cross_mio_corr/{0}_run{1}_{2}_fb_cur_{3}_fix_cross.txt".format(subj, r, t, fb), event_fixation_cross_norisk, fmt="%s")
+                        f = "/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/fix_cross_mio_corr{0}/{1}_run{2}_{3}_fb_cur_{4}_fix_cross.txt".format(prefix, subj, r, t, fb)
+                        np.savetxt("/net/server/data/Archive/prob_learn/asmyasnikova83/Events_autists/fix_cross_mio_corr{0}/{1}_run{2}_{3}_fb_cur_{4}_fix_cross.txt".format(prefix, subj, r, t, fb), event_fixation_cross_norisk, fmt="%s")
                     if Normals:
-                        f = "/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/fix_cross_mio_corr/{0}_run{1}_{2}_fb_cur_{3}_fix_cross.txt".format(subj, r, t, fb)
-                        np.savetxt("/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/fix_cross_mio_corr/{0}_run{1}_{2}_fb_cur_{3}_fix_cross.txt".format(subj, r, t, fb), event_fixation_cross_norisk, fmt="%s")
+                        f = "/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/fix_cross_mio_corr{0}/{1}_run{2}_{3}_fb_cur_{4}_fix_cross.txt".format(prefix, subj, r, t, fb)
+                        np.savetxt("/net/server/data/Archive/prob_learn/asmyasnikova83/Events_normals/fix_cross_mio_corr{0}/{1}_run{2}_{3}_fb_cur_{4}_fix_cross.txt".format(prefix, subj, r, t, fb), event_fixation_cross_norisk, fmt="%s")
                     print('Saved!')
-                    print(f)
                 except OSError:
                    print('This file not exist')
                     
