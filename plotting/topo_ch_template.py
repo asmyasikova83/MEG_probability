fab861faa88a1604c3b371c9202c25775713473a
asmyasikova83 <asmyasnikova83@gmail.com>
2022-09-15 23:25:02 +0300

    sensor maps for effects across samples

--- plotting/topo_ch_template.py
+++ plotting/topo_ch_template.py
@@ -9,22 +9,22 @@ from function import nocolor_topomaps_line
 
 #we will mark significant channels
 template_for_channels = False
-anterior_cluster = True
+anterior_cluster = False
 occipital_cluster = False
 decision = False
 post_response = False
-post_response_raw = False
 post_feedback_early = False
 post_error = False
-whole = False
-early_fb_cluster_trial = False
-early_fb_cluster_interaction = False
 by_one_fr = False
 by_one = False
-wide_decision = False
 super_decision  = False
 super_feedback = False
 super_early_feedback = False
+group =  False
+group_sign = False
+LFB_groups = False
+decision_groups = False
+EFB_groups = True
 
 n = 1 # количество говов в ряду
 #set time for the pic  plotting
@@ -59,41 +59,63 @@ if by_one:
     cluster[18,:] = 1
     cluster[19,:] = 1
     title = 'post_err'
-if early_fb_cluster_interaction:     
-    cluster[35,:] = 1
-    cluster[39,:] = 1
-    cluster[55,:] = 1
-    cluster[61,:] = 1
-    cluster[62,:] = 1
-    cluster[63,:] = 1
-    cluster[64,:] = 1
-    cluster[65,:] = 1
-    cluster[70,:] = 1
-    cluster[74,:] = 1
-    cluster[77,:] = 1
-    cluster[92,:] = 1
-    title = 'Early cluster interaction' 
-if early_fb_cluster_trial:     
-    cluster[11,:] = 1
-    cluster[42,:] = 1
-    cluster[50,:] = 1
+if group_sign:
+    cluster[10,:] = 1
+    cluster[26,:] = 1
+    cluster[27,:] = 1
+    cluster[34,:] = 1
+    cluster[59,:] = 1
     cluster[60,:] = 1
+    cluster[61,:] = 1
+    cluster[66,:] = 1
+    cluster[67,:] = 1
     cluster[68,:] = 1
     cluster[69,:] = 1
     cluster[70,:] = 1
     cluster[71,:] = 1
-    cluster[72,:] = 1
-    cluster[73,:] = 1
     cluster[74,:] = 1
     cluster[75,:] = 1
     cluster[76,:] = 1
     cluster[77,:] = 1
     cluster[78,:] = 1
-    cluster[79,:] = 1
-    cluster[88,:] = 1
+    cluster[84,:] = 1
+    cluster[85,:] = 1
     cluster[86,:] = 1
     cluster[89,:] = 1
-    title = 'Early fb cluster'
+    cluster[90,:] = 1
+    cluster[92,:] = 1
+    cluster[93,:] = 1
+    title = 'LFB'
+if group:
+    #cluster[6,:] = 1
+    # cluster[15,:] = 1
+    #cluster[16,:] = 1
+    cluster[36,:] = 1
+    cluster[38,:] = 1
+    cluster[39,:] = 1
+    cluster[40,:] = 1
+    cluster[41,:] = 1
+    cluster[46,:] = 1
+    cluster[49,:] = 1
+    #cluster[59,:] = 1
+    cluster[82,:] = 1
+    cluster[83,:] = 1
+    #cluster[15,:] = 1
+    #cluster[36,:] = 1
+    #cluster[38,:] = 1
+    #cluster[39,:] = 1
+    #cluster[40,:] = 1
+    #cluster[46,:] = 1
+    #cluster[59,:] = 1
+    #cluster[82,:] = 1
+    #cluster[15,:] = 1
+    #cluster[38,:] = 1
+    #cluster[40,:] = 1
+    #cluster[41,:] = 1
+    #cluster[59,:] = 1
+    #cluster[82,:] = 1
+    #cluster[83,:] = 1
+    title = 'Group_1400'
 if occipital_cluster:     
     cluster[60,:] = 1
     cluster[61,:] = 1
@@ -171,7 +193,6 @@ if post_error:
     cluster[8,:] = 1
     cluster[16,:] = 1
     cluster[19,:] = 1
-    
     title = 'Post-error'
 if post_feedback_early:
     cluster[60,:] = 1
@@ -185,30 +206,6 @@ if post_feedback_early:
     cluster[77,:] = 1
     cluster[86,:] = 1
     title = 'Post-fb early'
-if post_response_raw:
-
-    cluster[7,:] = 1
-    cluster[22,:] = 1
-    cluster[24,:] = 1
-    cluster[25,:] = 1
-    cluster[27,:] = 1
-    cluster[38,:] = 1
-    cluster[40,:] = 1
-    cluster[41,:] = 1
-    cluster[45,:] = 1
-    cluster[59,:] = 1
-    cluster[63,:] = 1
-    cluster[66,:] = 1
-    cluster[67,:] = 1
-    cluster[68,:] = 1
-    cluster[69,:] = 1
-    cluster[70,:] = 1
-    cluster[74,:] = 1
-    cluster[75,:] = 1
-    cluster[77,:] = 1
-    cluster[82,:] = 1
-    cluster[85,:] = 1
-    title = 'Post-resp_raw'
 if post_response:
     #cluster[7,:] = 1
     cluster[12,:] = 1
@@ -237,92 +234,29 @@ if post_response:
     cluster[83,:] = 1
     cluster[85,:] = 1
     title = 'Post-resp'
-if whole:
-    cluster[5,:] = 1
-    cluster[6,:] = 1
-    cluster[9,:] = 1
-    cluster[10,:] = 1
-    cluster[11,:] = 1
-    cluster[12,:] = 1
-    cluster[13,:] = 1
-    cluster[15,:] = 1
-    cluster[22,:] = 1
-    cluster[25,:] = 1
-    cluster[26,:] = 1
-    cluster[37,:] = 1
-    cluster[59,:] = 1
-    cluster[60,:] = 1 
-    cluster[61,:] = 1
+if LFB_groups:
+    #cluster[51,:] = 1
     cluster[63,:] = 1
     cluster[64,:] = 1
-    cluster[66,:] = 1
-    cluster[69,:] = 1
-    cluster[70,:] = 1
     cluster[71,:] = 1
-    cluster[74,:] = 1
-    cluster[75,:] = 1
     cluster[76,:] = 1
-    cluster[77,:] = 1
     cluster[78,:] = 1
-    cluster[84,:] = 1
-    cluster[86,:] = 1
-    cluster[92,:] = 1
-    cluster[93,:] = 1
-    title = 'Whole FB' 
-if wide_decision:
-    cluster[5,:] = 1
-    cluster[6,:] = 1
-    cluster[9,:] = 1
-    cluster[10,:] = 1
-    cluster[12,:] = 1
-    cluster[13,:] = 1
-    cluster[14,:] = 1
-    cluster[15,:] = 1
-    cluster[21,:] = 1
-    cluster[22,:] = 1
-    cluster[24,:] = 1
-    cluster[26,:] = 1
-    cluster[27,:] = 1
-    cluster[36,:] = 1 
-    cluster[37,:] = 1
+    #cluster[100,:] = 1
+    title = 'LFB_groups'
+if EFB_groups:
+    cluster[52,:] = 1
+    cluster[99,:] = 1
+    cluster[100,:] = 1
+    title = 'EFB_groups'
+if decision_groups:
     cluster[38,:] = 1
     cluster[39,:] = 1
-    cluster[40,:] = 1
-    cluster[41,:] = 1
-    cluster[45,:] = 1
-    cluster[59,:] = 1
-    cluster[60,:] = 1
-    cluster[64,:] = 1
-    cluster[66,:] = 1
-    cluster[67,:] = 1
-    cluster[68,:] = 1
-    cluster[69,:] = 1
-    cluster[70,:] = 1
-    cluster[71,:] = 1
-    cluster[72,:] = 1
-    cluster[73,:] = 1
-    cluster[74,:] = 1
-    cluster[75,:] = 1
-    cluster[76,:] = 1
-    cluster[77,:] = 1
-    cluster[78,:] = 1
-    cluster[79,:] = 1
-    cluster[82,:] = 1
-    cluster[83,:] = 1
-    cluster[84,:] = 1
-    cluster[85,:] = 1
-    cluster[86,:] = 1
-    cluster[87,:] = 1
-    cluster[88,:] = 1
-    cluster[89,:] = 1
-    cluster[93,:] = 1
-    cluster[94,:] = 1
-    title = 'wide decision'
-if super_decision:
-    cluster[68,:] = 1
-    cluster[75,:] = 1
-    cluster[76,:] = 1
-    title = 'super_decision'
+    cluster[43,:] = 1
+    cluster[46,:] = 1
+    cluster[53,:] = 1
+    cluster[98,:] = 1
+    #cluster[100,:] = 1
+    title = 'Decis_groups'
 if super_feedback:
     ##super posterior
     cluster[60,:] = 1
@@ -342,6 +276,5 @@ if super_early_feedback:
 
 stat_sensors = np.array(cluster)
 print(title)
-#os.makedirs('/net/server/data/Archive/prob_learn/asmyasnikova83/head_templates_channels_green/', exist_ok = True)
 fig = temp.plot_topomap(times = time_to_plot, ch_type='planar1', scalings = 1, units = 'dB', show = False, vmin = -0.9, vmax = 0.9, time_unit='s', title = title, colorbar = False, extrapolate = "local", mask = np.bool_(stat_sensors), mask_params = dict(marker='o', markerfacecolor='green', markeredgecolor='k', linewidth=0, markersize=10, markeredgewidth=2))
 fig.savefig(f'/net/server/data/Archive/prob_learn/asmyasnikova83/maps_signif_sensors/{title}.jpeg', dpi = 900)
